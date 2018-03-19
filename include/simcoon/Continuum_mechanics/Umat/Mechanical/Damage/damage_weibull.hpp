596b9ad4bccd4cc6127be7485b48f888b12cc374
chemisky <yves.chemisky@gmail.com>
2018-03-19 15:23:18 +0100

    Added damage_weibull subroutine

---
+++ include/simcoon/Continuum_mechanics/Umat/Mechanical/Damage/damage_weibull.hpp
@@ -0,0 +1,31 @@
+/* This file is part of simcoon.
+ 
+ simcoon is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ simcoon is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with simcoon.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+///@file damage_LLD_0.hpp
+///@brief Ladeveze-Le Dantec model that accounts for anisotropic damage and plasticity
+///@brief Damage evolution is considered as a function of time
+
+#pragma once
+
+#include <iostream>
+#include <armadillo>
+
+namespace simcoon{
+    
+void umat_damage_weibull(const arma::vec &, const arma::vec &, arma::vec &, arma::mat &, arma::mat &, arma::vec &, const arma::mat &, const int &, const arma::vec &, const int &, arma::vec &, const double &, const double &,const double &,const double &, double &, double &, double &, double &, const int &, const int &, const bool &, const int &, double &);
+    
+} //namespace simcoon
