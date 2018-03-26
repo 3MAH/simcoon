a8f002fccec5ac1273fd0775d60d2365b69cb495
chemisky <yves.chemisky@gmail.com>
2018-03-26 23:16:55 +0200

    Corrected header set_equation

--- include/simcoon/Continuum_mechanics/Unit_cell/interpolate.hpp
+++ include/simcoon/Continuum_mechanics/Unit_cell/interpolate.hpp
@@ -35,6 +35,6 @@ void find_neighbours_set(std::vector<std::vector<Node> > &, std::vector<arma::ma
     
 void set_weights(std::vector<arma::mat> &, const std::vector<arma::mat> &, const unsigned int &, const double &);
     
-equation set_equation(const std::vector<Node> &neigh, const arma::mat &weight, const unsigned int &, const unsigned int &);
+equation set_equation(const std::vector<Node> &neigh, const arma::mat &weight, const unsigned int &, const unsigned int &, const unsigned int &);
 
 } //namespace simcoon
