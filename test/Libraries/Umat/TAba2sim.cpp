4ad7d23e2e80b90d0126718f24e8c89a15de6887
chemisky <yves.chemisky@gmail.com>
2018-07-05 09:27:17 +0200

    upodate Aba2simcoon test with random numbers

--- test/Libraries/Umat/TAba2sim.cpp
+++ test/Libraries/Umat/TAba2sim.cpp
@@ -28,10 +28,12 @@
 #include <armadillo>
 #include <simcoon/parameter.hpp>
 #include <simcoon/Continuum_mechanics/Umat/umat_smart.hpp>
+#include <simcoon/Simulation/Maths/random.hpp>
 #include <simcoon/Simulation/Phase/phase_characteristics.hpp>
 #include <simcoon/Simulation/Phase/state_variables.hpp>
 #include <simcoon/Simulation/Phase/state_variables_M.hpp>
 #include <simcoon/Simulation/Solver/read.hpp>
+
 
 using namespace std;
 using namespace arma;
@@ -39,6 +41,9 @@ using namespace simcoon;
 
 BOOST_AUTO_TEST_CASE( read_write )
 {
+
+    /* initialize random seed: */
+    srand(time(NULL));
     
     string path_data = "data";
     string materialfile = "material.dat";    
@@ -54,6 +59,9 @@ BOOST_AUTO_TEST_CASE( read_write )
     
     //Characterisitcs for the statecv test
     double *statev = new double[nstatev];
+    for (int i=0; i<nstatev; i++) {
+        statev[i] = alead(0.,100.);
+    }
     double *stress = new double[6];
     double *stran = new double[6];
     double *dstran = new double[6];
@@ -63,7 +71,7 @@ BOOST_AUTO_TEST_CASE( read_write )
     double temperature = 273.15;
     double Dtemperature = 0.;
     unsigned int nprops = 3;
-    vec props = zeros(3);
+    double *props = new double[3];
     props[0] = 70000.;
     props[1] = 0.3;
     props[2] = 1.E-5;
@@ -80,9 +88,10 @@ BOOST_AUTO_TEST_CASE( read_write )
     bool start = false;
     double Time = 0.;
     double DTime = 0.;
-    int solver_type = 0;
+    //    int solver_type = 0;
     
-    unsigned int nstatev_smart = 0;
+    unsigned int nstatev_multi = 0;
+    unsigned int nstatev_macro = 0;
     vec props_smart = zeros(nprops);
     
     vec sigma = zeros(6);
@@ -94,33 +103,30 @@ BOOST_AUTO_TEST_CASE( read_write )
     
     string umat_name(cmname);
     umat_name = umat_name.substr(0, 5);
-
-    read_matprops(umat_name, nprops, props, nstatev_smart, psi_rve, theta_rve, phi_rve, path_data, materialfile);
-    vec statev_smart = zeros(nstatev_smart);
     
-    size_statev(rve, nstatev_smart);
-    cout << "nstatev_smart = " << nstatev_smart << endl;
+    size_statev(rve, nstatev_multi);
+    cout << "nstatev_multi = " << nstatev_multi << endl;
     
-    nstatev += 4;
+    nstatev = nstatev_multi + nstatev_macro + 4;
     cout << "nstatev = " << nstatev << endl;
     
-    vec statev_smart_n = statev_smart;
-//    std::copy(std::begin(statev_n), std::end(statev_n), std::begin(statev_n));
-    
     auto rve_sv_M = std::dynamic_pointer_cast<state_variables_M>(rve.sptr_sv_global);
-    rve_sv_M->resize(nstatev_smart);
+    rve_sv_M->resize(nstatev_macro);
     rve.sptr_matprops->resize(nprops);
+
+    vec statev_macro = zeros(nstatev_macro);
+    vec statev_multi(&statev[nstatev_macro+4], nstatev_multi, false, false);
+    vec statev_multi_n = statev_multi;
     
     unsigned int pos = 0;
-    statev_2_phases(rve, pos, statev_smart);
+    statev_2_phases(rve, pos, statev_multi);
     
-//    abaqus2smart_M(stress, ddsdde, stran, dstran, time, dtime, temperature, Dtemperature, nprops, props, nstatev, statev, ndi, nshr, drot, rve_sv_M->sigma, rve_sv_M->Lt, rve_sv_M->Etot, rve_sv_M->DEtot, rve_sv_M->T, rve_sv_M->DT, Time, DTime, props_smart, rve_sv_M->Wm, rve_sv_M->statev, DR, start);
+    abaqus2smart_M(stress, ddsdde, stran, dstran, time, dtime, temperature, Dtemperature, nprops, props, nstatev_macro, statev, ndi, nshr, drot, rve_sv_M->sigma, rve_sv_M->Lt, rve_sv_M->Etot, rve_sv_M->DEtot, rve_sv_M->T, rve_sv_M->DT, Time, DTime, props_smart, rve_sv_M->Wm, rve_sv_M->statev, DR, start);
+    smart2abaqus_M(stress, ddsdde, statev, ndi, nshr, rve_sv_M->sigma, rve_sv_M->statev, rve_sv_M->Wm, rve_sv_M->Lt);
     
-//    smart2abaqus_M(stress, ddsdde, statev, ndi, nshr, rve_sv_M->sigma, rve_sv_M->statev, rve_sv_M->Wm, rve_sv_M->Lt);
+    phases_2_statev(statev_multi, pos, rve);
     
-    phases_2_statev(statev_smart, pos, rve);
-    
-    BOOST_CHECK_EQUAL_COLLECTIONS(statev_smart.begin(), statev_smart.end(), statev_smart_n.begin(), statev_smart_n.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(statev_multi.begin(), statev_multi.end(), statev_multi_n.begin(), statev_multi_n.end());
     
     delete[] statev;
     delete[] stress;
