937134398f932e86b6b6c9c2e6e7857afc50de39
chemisky <yves.chemisky@gmail.com>
2018-07-06 14:05:38 +0200

    changed logo

--- docs/conf.py
+++ docs/conf.py
@@ -150,7 +150,7 @@ html_theme = 'sphinxdoc'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = '_static/simcoon_logo_small_u.png'
+html_logo = '_static/simcoon_logo_small.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
