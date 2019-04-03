c9e57b38b37e934df3b09a62dd004723bde45e10
chemisky <yves.chemisky@gmail.com>
2019-04-03 12:17:30 +0200

    changed name of generation of kinematical boudary conditions

--- include/simcoon/Simulation/Solver/step_thermomeca.hpp
+++ include/simcoon/Simulation/Solver/step_thermomeca.hpp
@@ -55,6 +55,7 @@ protected:
     
     using step::generate;
     virtual void generate(const double&, const arma::vec&, const arma::vec&, const double&);
+    virtual void generate_kin(const double&, const arma::mat&m, const double &);    
     virtual void assess_inc(const double &, double &, const double &, phase_characteristics &, double &, const double &);    
     
     virtual step_thermomeca& operator = (const step_thermomeca&);
