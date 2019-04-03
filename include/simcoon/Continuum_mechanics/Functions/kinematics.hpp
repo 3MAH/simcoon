ec4c92172555f3b95b0df51ce3f97237ba92bf89
chemisky <yves.chemisky@gmail.com>
2019-04-03 11:50:14 +0200

    Corrected declaration bug in several functions

--- include/simcoon/Continuum_mechanics/Functions/kinematics.hpp
+++ include/simcoon/Continuum_mechanics/Functions/kinematics.hpp
@@ -25,13 +25,13 @@
 namespace simcoon{
 
 //This function returns the deviatoric part of m
-arma::mat dev(const arma::mat &m);
+arma::mat dev(const arma::mat &);
 
 //This function returns the spherical part of m
-arma::mat sph(const arma::mat &m);
+arma::mat sph(const arma::mat &);
 
 //This function returns F (in a vectorized), from E (Green-Lagrange strain) and R (Rotation matrix), according to a RU decomposition
-arma::vec ER_to_F(const mat&E, const mat&R)
+arma::vec ER_to_F(const arma::mat&, const arma::mat&);
 
 //This function computes the gradient of displacement (Lagrangian) from the deformation gradient tensor
 arma::mat G_UdX(const arma::mat &);
