24c072c8a48830e0a0ed45511c011901c8aefa5b
chemisky <yves.chemisky@gmail.com>
2019-07-19 11:33:10 +0200

    Added a Chaboche plastic law with 2 non-linear isotropic laws

---
+++ include/simcoon/Continuum_mechanics/Umat/Mechanical/Plasticity/plastic_chaboche_ccp.hpp
@@ -0,0 +1,50 @@
+/* This file is part of simcoon.
+ 
+ simcoon is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ simcoon is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with simcoon.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+///@file plastic_isotropic_ccp.hpp
+///@brief User subroutine for elastic-plastic materials in 1D-2D-3D case
+///@brief This subroutines uses a convex cutting plane algorithm
+///@brief Isotropic hardening with a power-law hardenig is considered
+///@version 1.0
+
+#pragma once
+#include <armadillo>
+
+namespace simcoon{
+
+///@brief The elastic-plastic UMAT with isotropic hardening requires 6 constants:
+
+///@brief props[1] : Young modulus
+///@brief props[2] : Poisson ratio
+///@brief props[3] : CTE
+///@brief props[4] : J2 equivalent yield stress limit : sigmaY
+///@brief props[5] : hardening parameter k
+///@brief props[6] : exponent m
+
+///@brief The elastic-plastic UMAT with isotropic hardening requires 8 statev:
+///@brief statev[0] : T_init : Initial temperature
+///@brief statev[1] : Accumulative plastic parameter: p
+///@brief statev[2] : Plastic strain 11: EP(0,0)
+///@brief statev[3] : Plastic strain 22: EP(1,1)
+///@brief statev[4] : Plastic strain 33: EP(2,2)
+///@brief statev[5] : Plastic strain 12: EP(0,1)
+///@brief statev[6] : Plastic strain 13: EP(0,2)
+///@brief statev[7] : Plastic strain 23: EP(1,2)
+
+void umat_plasticity_chaboche_CCP(const arma::vec &, const arma::vec &, arma::vec &, arma::mat &, arma::mat &, arma::vec &, const arma::mat &, const int &, const arma::vec &, const int &, arma::vec &, const double &, const double &,const double &,const double &, double &, double &, double &, double &, const int &, const int &, const bool &, const int &, double &);
+                                
+} //namespace simcoon
