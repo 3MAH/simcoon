6edee61c6208d94b426c6576dfae8f31dd8384a2
chemisky <yves.chemisky@gmail.com>
2019-07-19 11:38:26 +0200

    started test log strain implementation

--- test/Umats/LOG_int/TLOG_int.cpp
+++ test/Umats/LOG_int/TLOG_int.cpp
@@ -31,6 +31,9 @@
 #include <armadillo>
 #include <simcoon/parameter.hpp>
 #include <simcoon/Continuum_mechanics/Umat/umat_smart.hpp>
+#include <simcoon/Continuum_mechanics/Functions/transfer.hpp>
+#include <simcoon/Continuum_mechanics/Functions/kinematics.hpp>
+#include <simcoon/Continuum_mechanics/Functions/objective_rates.hpp>
 #include <simcoon/Simulation/Solver/read.hpp>
 #include <simcoon/Simulation/Solver/block.hpp>
 #include <simcoon/Simulation/Solver/step.hpp>
@@ -79,20 +82,35 @@ BOOST_AUTO_TEST_CASE( TLOG_int_solver )
     string path_comparison = "comparison/results_job_global-0.txt";
     string path_outputfile = path_results + "/" + "results_job_global-0.txt";
     
-    mat C;
-    C.load(path_comparison);
-
     mat R;
     R.load(path_outputfile);
     
-    for (unsigned int i=0; i<C.n_rows; i++) {
-        for (unsigned int j=0; j<C.n_cols; j++) {
-            if (fabs(C(i,j)) > sim_iota) {
-                BOOST_CHECK( pow(pow(C(i,j),2.) - pow(R(i,j),2.),0.5)/fabs(C(i,j)) < 1.E-6 );
-            }
-            else {
-                BOOST_CHECK( pow(pow(C(i,j),2.) - pow(R(i,j),2.),0.5) < 1.E-6 );
-            }
-        }
-    }
+    //From the imposed transformation tensor,
+    mat F_test = zeros(3,3);
+    
+	F_test(0,0) = 1.;
+	F_test(0,1) = 5.;
+	F_test(0,2) = 2.;
+	F_test(1,0) = 0.;
+	F_test(1,1) = 2.;
+	F_test(1,2) = 0.;
+	F_test(2,0) = 0.;
+	F_test(2,1) = 0.;
+	F_test(2,2) = 0.;
+    
+    vec e_tot_log_test = t2v_strain(0.5*logmat_sympd(L_Cauchy_Green(F_test)));
+    double n_rows_results = R.n_rows;
+    vec e_tot_log = zeros(6);
+    
+    e_tot_log(0) = R(8,n_rows_results-1);
+    e_tot_log(1) = R(9,n_rows_results-1);
+    e_tot_log(2) = R(10,n_rows_results-1);
+    e_tot_log(2) = R(11,n_rows_results-1);
+    e_tot_log(4) = R(12,n_rows_results-1);
+    e_tot_log(5) = R(13,n_rows_results-1);
+
+    cout << "e_tot_log_test = " << e_tot_log_test.t();
+    cout << "e_tot_log = " << e_tot_log_test.t();
+    BOOST_CHECK( norm(e_tot_log_test - e_tot_log,2) < 1.E-9 );
+    
 }
