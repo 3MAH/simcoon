c2495c159790112d37713c5d2a5c93f39e8d7ac4
chemisky <yves.chemisky@gmail.com>
2019-07-19 11:29:18 +0200

    modified L_eff according to new simcoon function parameters

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/Leff.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/Leff.hpp
@@ -5,6 +5,6 @@
 namespace simpy{
 
 //return the elastic stiffness tensor of a composite material
-    boost::python::numpy::ndarray L_eff(const boost::python::str &, const boost::python::numpy::ndarray &, const int &, const double &, const double &, const double &, const boost::python::str &);
+boost::python::numpy::ndarray L_eff(const boost::python::str &, const boost::python::numpy::ndarray &, const int &, const double &, const double &, const double &, const boost::python::str &);
     
 } //namespace simpy
