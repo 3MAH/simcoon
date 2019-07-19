9633b94141bbf9e95b4b1ee904101ccd6d1315ae
chemisky <yves.chemisky@gmail.com>
2019-07-19 11:29:26 +0200

    Added a Unit_Cell wrapper

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Unit_cell/unit_cell.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Unit_cell/unit_cell.hpp
@@ -1,6 +1,18 @@
 #pragma once
 #include <boost/python.hpp>
+#include <simcoon/Continuum_mechanics/Unit_cell/node.hpp>
+#include <simcoon/Continuum_mechanics/Unit_cell/cubic_mesh.hpp>
 
 namespace simpy{
+    
+simcoon::Node build_node(const int &, const boost::python::numpy::ndarray &);
+
+boost::python::numpy::ndarray readwrite_coords(simcoon::Node &);
+    
+void get_domain(simcoon::cubic_mesh);
+
+void construct_lists(simcoon::cubic_mesh &);
+    
+//simcoon::cubic_mesh build_cubic_mesh(const boost::python::list &, const boost::python::list &);
     
 } //namespace simpy
