4fe4ed667b9ce19bf17d80117c4e77297a61b049
chemisky <yves.chemisky@gmail.com>
2019-07-19 11:34:52 +0200

    Small bug fix

--- software/L_eff.cpp
+++ software/L_eff.cpp
@@ -57,8 +57,8 @@ int main() {
     read_matprops(umat_name, nprops, props, nstatev, psi_rve, theta_rve, phi_rve, path_data, materialfile);
     phase_characteristics rve;
     
-    rve.sptr_matprops->update(0, umat_name, 1, psi_rve, theta_rve, phi_rve, props.n_elem, props);
     rve.construct(0,1);
+    rve.sptr_matprops->update(0, umat_name, 1, psi_rve, theta_rve, phi_rve, props.n_elem, props);
     rve.sptr_sv_global->update(zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(3,3), zeros(3,3), eye(3,3), eye(3,3),T_init, 0., nstatev, zeros(nstatev), zeros(nstatev));
     
     auto sv_M = std::dynamic_pointer_cast<state_variables_M>(rve.sptr_sv_global);
