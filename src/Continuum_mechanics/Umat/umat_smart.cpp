203e44aa12709fe429efc813e5fd359fcafeccfc
chemisky <yves.chemisky@gmail.com>
2019-08-23 15:33:15 +0200

    added a select_umat for finite strain

--- src/Continuum_mechanics/Umat/umat_smart.cpp
+++ src/Continuum_mechanics/Umat/umat_smart.cpp
@@ -840,6 +840,34 @@ void select_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,
     rve.local2global();
     
 }
+
+void select_umat_M_finite(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, const int &solver_type, double &tnew_dt)
+{
+
+    std::map<string, int> list_umat;
+    
+    list_umat = {{"UMEXT",0},{"ELISO",1},{"ELIST",2},{"ELORT",3},{"EPICP",4},{"EPKCP",5},{"EPCHA",6},{"SMAUT",7},{"LLDM0",8},{"ZENER",10},{"ZENNK",11},{"PRONK",12},{"EPHIC",17},{"EPHIN",18},{"SMAMO",19},{"SMAMC",20},{"MIHEN",100},{"MIMTN",101},{"MISCN",103},{"MIPLN",104}};
+    
+        rve.global2local();
+        auto umat_M = std::dynamic_pointer_cast<state_variables_M>(rve.sptr_sv_local);
+    
+        cout << "rve.sptr_matprops->umat_name = " << rve.sptr_matprops->umat_name << endl;
+    
+        switch (list_umat[rve.sptr_matprops->umat_name]) {
+
+            case 0: {
+//                umat_external(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
+                break;
+            }
+            case 1: {
+                umat_elasticity_iso(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
+                 break;
+             }
+        }
+        umat_M->PKII = t2v_strain(Kirchoff2PKII(v2t_strain(umat_M->sigma), umat_M->F1));
+        rve.local2global();
+                
+}
     
 void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, const bool &start, const int &solver_type, double &tnew_dt)
 {
@@ -851,6 +879,8 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
         rve.global2local();
         auto umat_M = std::dynamic_pointer_cast<state_variables_M>(rve.sptr_sv_local);
     
+        cout << "rve.sptr_matprops->umat_name = " << rve.sptr_matprops->umat_name << endl;
+    
         switch (list_umat[rve.sptr_matprops->umat_name]) {
 
             case 0: {
@@ -858,8 +888,7 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
                 break;
             }
             case 1: {
-                umat_elasticity_iso(umat_M->etot, umat_M->Detot, umat_M->tau, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
-                    umat_M->sigma = Kirchoff2Cauchy(umat_M->tau, umat_M->F1);
+                umat_elasticity_iso(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
                  break;
              }
              case 2: {
@@ -927,10 +956,7 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
                 exit(0);
             }
         }
-        umat_M->PKII = t2v_strain(Kirchoff2PKII(v2t_strain(umat_M->sigma), umat_M->F1));
         rve.local2global();
-    
-
 }
     
 void run_umat_T(phase_characteristics &rve, const mat &DR,const double &Time,const double &DTime, const int &ndi, const int &nshr, bool &start, const int &solver_type, double &tnew_dt)
