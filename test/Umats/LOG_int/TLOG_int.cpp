525e1565236e93f5cb72658950eccfb5d5989b41
chemisky <yves.chemisky@gmail.com>
2020-01-12 22:48:00 +0100

    cosmetics

--- test/Umats/LOG_int/TLOG_int.cpp
+++ test/Umats/LOG_int/TLOG_int.cpp
@@ -75,10 +75,11 @@ BOOST_AUTO_TEST_CASE( TLOG_int_solver )
     
     solver_essentials(solver_type, path_data, sol_essentials);
     solver_control(div_tnew_dt_solver, mul_tnew_dt_solver, miniter_solver, maxiter_solver, inforce_solver, precision_solver, lambda_solver, path_data, sol_control);
-    
+
     read_matprops(umat_name, nprops, props, nstatev, psi_rve, theta_rve, phi_rve, path_data, materialfile);
-    solver(umat_name, props, nstatev, psi_rve, theta_rve, phi_rve, solver_type, div_tnew_dt_solver, mul_tnew_dt_solver, miniter_solver, maxiter_solver, inforce_solver, precision_solver, lambda_solver, path_data, path_results, pathfile, outputfile);
     
+    solver(umat_name, props, nstatev, psi_rve, theta_rve, phi_rve, solver_type, div_tnew_dt_solver, mul_tnew_dt_solver, miniter_solver, maxiter_solver, inforce_solver, precision_solver, lambda_solver, path_data, path_results, pathfile, outputfile);
+
     string path_outputfile = path_results + "/" + "results_job_global-0.txt";
     mat R;
     R.load(path_outputfile);
