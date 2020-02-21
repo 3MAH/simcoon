569b8311ed3e95950a8795a3f490a63e58d36805
Yves Chemisky <yves.chemisky@gmail.com>
2020-02-21 23:02:15 +0100

    Update read.hpp
    added abaqus parser

--- include/simcoon/Continuum_mechanics/Unit_cell/read.hpp
+++ include/simcoon/Continuum_mechanics/Unit_cell/read.hpp
@@ -35,12 +35,14 @@ namespace simcoon{
 /// Function that splits a line using delimiters
 std::vector<std::string> split(const std::string &, const char & = ',');
    
-//void unit_cell_essentials(unsigned int &, unsigned int &, int &, const std::string & = "data", const std::string & = "unit_cell_essentials.inp");
+void unit_cell_essentials(unsigned int &, unsigned int &, unsigned int &, unsigned int &, const std::string & = "data", const std::string & = "unit_cell_essentials.inp");
 
 /// Function that reads the characteristics of a phase
 void read_sections(std::vector<section_characteristics> &, const unsigned int &, const std::string & = "data", const std::string & = "Nsections0.dat");
 
-void read_salome_full_mesh(std::vector<section_characteristics> &, std::vector<arma::Col<int> > &, const unsigned int &, const std::string & = "data", const std::string & = "Nsections0.dat", const std::string & = "sc_mesh.dat", const std:: string & = "mesh.dat");
+void read_abaqus_full_mesh(std::vector<section_characteristics> &, std::vector<Node> &, std::vector<Element> &, std::vector<std::vector<std::string> > &, const unsigned int &, const std::vector<std::string> &, const std::string & = "data", const std::string & = "Nsections.dat", const std::string & = "sc_mesh.dat", const std::string & = "mesh.inp");
+
+void read_salome_full_mesh(std::vector<section_characteristics> &, std::vector<arma::Col<int> > &, const unsigned int &, const std::string & = "data", const std::string & = "Nsections.dat", const std::string & = "sc_mesh.dat", const std:: string & = "mesh.dat");
 
 void read_salome_mesh(std::vector<Node> &, std::vector<Element> &, const std::string & = "data", const std:: string & = "mesh.dat");
 
