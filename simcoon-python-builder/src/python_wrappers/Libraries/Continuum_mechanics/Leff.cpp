34ba4416c3970e789ef97b3962003ed88499d699
Yves Chemisky <yves.chemisky@gmail.com>
2021-02-01 14:57:23 +0100

    Update Leff.cpp

--- simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/Leff.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/Leff.cpp
@@ -4,11 +4,13 @@
 #include <boost/python/numpy.hpp>
 #include <simcoon/arma2numpy/numpy_arma.hpp>
 
+#include <simcoon/Continuum_mechanics/Functions/natural_basis.hpp>
 #include <simcoon/Simulation/Phase/phase_characteristics.hpp>
 #include <simcoon/Simulation/Phase/state_variables_M.hpp>
 #include <simcoon/Continuum_mechanics/Umat/umat_L_elastic.hpp>
 
 #include <simcoon/python_wrappers/Libraries/Continuum_mechanics/Leff.hpp>
+
 
 namespace bp = boost::python;
 namespace bn = boost::python::numpy;
@@ -30,7 +32,8 @@ bn::ndarray L_eff(const std::string &umat_name_py, const bn::ndarray &props_py, 
     simcoon::phase_characteristics rve;
     rve.sptr_matprops->update(0, umat_name_py, 1, psi_rve, theta_rve, phi_rve, props.n_elem, props);
     rve.construct(0,1);
-    rve.sptr_sv_global->update(zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), eye(3,3), eye(3,3), eye(3,3), eye(3,3), T_init, 0., nstatev, zeros(nstatev), zeros(nstatev));
+    simcoon::natural_basis nb;
+    rve.sptr_sv_global->update(zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(6), zeros(3,3), zeros(3,3), eye(3,3), eye(3,3),T_init, 0., nstatev, zeros(nstatev), zeros(nstatev), nb);
     
     auto sv_M = std::dynamic_pointer_cast<simcoon::state_variables_M>(rve.sptr_sv_global);
     
