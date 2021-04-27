ab75705ffbbbfebadfaf30d06b22e4f2cd8c6440
Yves Chemisky <yves.chemisky@gmail.com>
2021-04-27 21:54:11 +0200

    fix tabulation

--- include/simcoon/Continuum_mechanics/Unit_cell/cubic_equation.hpp
+++ include/simcoon/Continuum_mechanics/Unit_cell/cubic_equation.hpp
@@ -49,37 +49,37 @@ class cubic_equation
     std::vector<std::string> CD_set_name;
     std::vector<int> list_dofs;
     
-        std::vector<equation> Corner_listXmYpZm;
-        std::vector<equation> Corner_listXpYmZm;
-        std::vector<equation> Corner_listXpYpZm;
-        std::vector<equation> Corner_listXmYmZp;
-        std::vector<equation> Corner_listXmYpZp;
-        std::vector<equation> Corner_listXpYmZp;
-        std::vector<equation> Corner_listXpYpZp;
-    
-        std::vector<std::vector<equation> > Edge_listXpYm;
-        std::vector<std::vector<equation> > Edge_listXpYp;
-        std::vector<std::vector<equation> > Edge_listXmYp;
-        std::vector<std::vector<equation> > Edge_listXpZm;
-        std::vector<std::vector<equation> > Edge_listXpZp;
-        std::vector<std::vector<equation> > Edge_listXmZp;
-        std::vector<std::vector<equation> > Edge_listYpZm;
-        std::vector<std::vector<equation> > Edge_listYpZp;
-        std::vector<std::vector<equation> > Edge_listYmZp;
-        std::vector<std::vector<equation> > Face_listXp;
-        std::vector<std::vector<equation> > Face_listYp;
-        std::vector<std::vector<equation> > Face_listZp;
+    std::vector<equation> Corner_listXmYpZm;
+    std::vector<equation> Corner_listXpYmZm;
+    std::vector<equation> Corner_listXpYpZm;
+    std::vector<equation> Corner_listXmYmZp;
+    std::vector<equation> Corner_listXmYpZp;
+    std::vector<equation> Corner_listXpYmZp;
+    std::vector<equation> Corner_listXpYpZp;
 
-        cubic_equation(); 	//default constructor
-        cubic_equation(const cubic_mesh &, const cubic_mesh &, const unsigned int &, const unsigned int &); 	//Parameter constructor
-        
-        cubic_equation(const cubic_equation &);	//Copy constructor
-        ~cubic_equation();
+    std::vector<std::vector<equation> > Edge_listXpYm;
+    std::vector<std::vector<equation> > Edge_listXpYp;
+    std::vector<std::vector<equation> > Edge_listXmYp;
+    std::vector<std::vector<equation> > Edge_listXpZm;
+    std::vector<std::vector<equation> > Edge_listXpZp;
+    std::vector<std::vector<equation> > Edge_listXmZp;
+    std::vector<std::vector<equation> > Edge_listYpZm;
+    std::vector<std::vector<equation> > Edge_listYpZp;
+    std::vector<std::vector<equation> > Edge_listYmZp;
+    std::vector<std::vector<equation> > Face_listXp;
+    std::vector<std::vector<equation> > Face_listYp;
+    std::vector<std::vector<equation> > Face_listZp;
+
+    cubic_equation(); 	//default constructor
+    cubic_equation(const cubic_mesh &, const cubic_mesh &, const unsigned int &, const unsigned int &); 	//Parameter constructor
     
-        void construct(const cubic_mesh &, const cubic_mesh &, const unsigned int &, const unsigned int &);
-        
-        //    virtual cubic_equation& operator = (const cubic_equation&);
-        friend std::ostream& operator << (std::ostream&, const cubic_equation&);
+    cubic_equation(const cubic_equation &);	//Copy constructor
+    ~cubic_equation();
+
+    void construct(const cubic_mesh &, const cubic_mesh &, const unsigned int &, const unsigned int &);
+    
+    //    virtual cubic_equation& operator = (const cubic_equation&);
+    friend std::ostream& operator << (std::ostream&, const cubic_equation&);
 };
     
 } //namespace simcoon
