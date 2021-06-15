f5ce28c4721fd95f64dd8a13fba7d49332eb1541
Yves Chemisky <yves.chemisky@gmail.com>
2021-06-15 22:14:56 +0200

    added read_path, updated read_matprops

--- simcoon-python-builder/src/python_wrappers/Libraries/Solver/read.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Solver/read.cpp
@@ -6,6 +6,7 @@
 
 #include <simcoon/Simulation/Solver/read.hpp>
 #include <simcoon/python_wrappers/Libraries/Solver/read.hpp>
+#include <simcoon/python_wrappers/Libraries/Solver/step_meca.hpp>
 
 namespace bp = boost::python;
 namespace bn = boost::python::numpy;
@@ -16,13 +17,46 @@ using namespace arma2numpy;
 namespace simpy {
 
 //This function reads material properties to prepare a simulation
-void read_matprops(unsigned int &nprops, bn::ndarray &props, unsigned int &nstatev, double &psi_rve, double &theta_rve, double &phi_rve, const std::string &path_data_py, const std::string &materialfile_py) {
-    vec v = array2vec(props);
+bp::tuple read_matprops(const std::string &path_data_py, const std::string &materialfile_py) {
+    unsigned int nprops;
+    unsigned int nstatev;
+    double psi_rve;
+    double theta_rve;
+    double phi_rve;
+    vec v;
     string umat_name;
 //    string path_data = bp::extract<std::string>(path_data_py);
 //    string materialfile = bp::extract<std::string>(materialfile_py);
     simcoon::read_matprops(umat_name, nprops, v, nstatev, psi_rve, theta_rve, phi_rve, path_data_py, materialfile_py);
-    props = vec2array(v);
+    return bp::make_tuple(nprops, nstatev, psi_rve, theta_rve, phi_rve, vec2array(v));
 }
+
+bp::tuple read_path(const std::string &path_data_py, const std::string &pathfile_py) {
+
+    double T;
+    bp::list blocks_py;
+    std::vector<simcoon::block> blocks;
+    
+//    string path_data = bp::extract<std::string>(path_data_py);
+//    string materialfile = bp::extract<std::string>(materialfile_py);
+    simcoon::read_path(blocks, T, path_data_py, pathfile_py);
+
+    //blocks loop
+    for(unsigned int i = 0 ; i < blocks.size() ; i++) {
+        bp::list ith_block_py;
+        //cycle loop
+        for(unsigned int n = 0; n < blocks[i].ncycle; n++) {
+            // Step loop
+            for(unsigned int j = 0; j < blocks[i].nstep; j++) {
+                shared_ptr<simcoon::step_meca> sptr_meca = std::dynamic_pointer_cast<simcoon::step_meca>(blocks[i].steps[j]);
+                step_meca_py stm_py(*sptr_meca);
+                ith_block_py.append(stm_py);
+            }
+        }
+        blocks_py.append(ith_block_py);
+    }
+    return bp::make_tuple(T, blocks_py);
+}
+
         
 } //namepsace simpy
