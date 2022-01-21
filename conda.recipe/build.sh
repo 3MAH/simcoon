24efb6ee4f1f0dbe0a11a6fe710620b4f322461c
Yves Chemisky <yves.chemisky@gmail.com>
2022-01-21 09:53:53 +0100

    test - update conda

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+#Find the current directory
+current_dir=$(pwd)
+
 mkdir -p build
 cd build
 cmake .. -DCMAKE_INCLUDE_PATH=$PREFIX/include -DCMAKE_LIBRARY_PATH=$PREFIX/lib -DCMAKE_INSTALL_PREFIX=$PREFIX -Wno-dev
@@ -17,8 +20,16 @@ make
 make install
 cd ..
 cp build/lib/simmit.so ../python-setup/simcoon/
-cp build/lib/simmit.so $PREFIX/lib
+#cp build/lib/simmit.so $PREFIX/lib
 cd ..
+
+if [ $OS = "Mac" ]
+then
+    install_name_tool -change libsimcoon.dylib @rpath/libsimcoon.dylib $PREFIX/lib/libarma2numpy.dylib
+    install_name_tool -change ${current_dir}/simcoon-python-builder/build/lib/libarma2numpy.dylib  @rpath/libarma2numpy.dylib $PREFIX/lib/python${python_version}/site-packages/simcoon/simmit.so
+    install_name_tool -change libsimcoon.dylib @rpath/libsimcoon.dylib $PREFIX/lib/python${python_version}/site-packages/simcoon/simmit.so
+fi
 
 cd python-setup
 pip install .
+
