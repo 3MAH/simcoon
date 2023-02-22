8bb7809b35a6ad9755a4013e9dcbabee1775a13e
Yves Chemisky <yves.chemisky@gmail.com>
2023-02-22 11:15:53 +0100

    Create hypoelastic_orthotropic for test on Eulerian constitutive laws (ongoing)

---
+++ include/simcoon/Continuum_mechanics/Umat/Finite/hypoelastic_orthotropic.hpp
@@ -0,0 +1,37 @@
+/* This file is part of simcoon.
+ 
+ simcoon is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ simcoon is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with simcoon.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+///@file elastic_orthotropic.hpp
+///@brief User subroutine for ortothropic elastic materials in 3D case
+///@version 1.0
+
+#pragma once
+
+#include <armadillo>
+
+namespace simcoon{
+
+///@brief The elastic UMAT requires 2 constants:
+///@brief props[0] : Young modulus
+///@brief props[1] : Poisson ratio
+///@brief props[2] : CTE
+
+///@brief No statev is required for thermoelastic constitutive law
+
+void umat_hypoelasticity_ortho(const arma::vec &, const arma::vec &, const arma::mat &, const arma::mat&, arma::vec &, arma::mat &, arma::mat &, arma::vec &, const arma::mat &, const int &, const arma::vec &, const int &, arma::vec &, const double &, const double &,const double &,const double &, double &, double &, double &, double &, const int &, const int &, const bool &, const int &, double &);
+                              
+} //namespace simcoon
