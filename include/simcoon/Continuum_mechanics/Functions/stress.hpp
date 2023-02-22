d52ee09521b0da0df450816e5e797f6500d3887a
Yves Chemisky <yves.chemisky@gmail.com>
2023-02-22 11:15:12 +0100

    Added doxygen documentation

--- include/simcoon/Continuum_mechanics/Functions/stress.hpp
+++ include/simcoon/Continuum_mechanics/Functions/stress.hpp
@@ -15,52 +15,301 @@
  
  */
 
-///@file stress.hpp
-///@brief A set of functions that transforms stress measures into anothers (considering Finite strains)
-///@version 1.0
-
 #pragma once
 #include <armadillo>
 
+
 namespace simcoon{
 
-//This function returns the first Piola-Kirchoff stress tensor from the Cauchy stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Cauchy2PKI(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+* @file stress.hpp
+* @author Yves Chemisky
+* @brief A set of functions that transforms stress measures into anothers (considering finite transformations)
+*/
 
-//This function returns the second Piola-Kirchoff stress tensor from the Cauchy stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Cauchy2PKII(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the first Piola Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ *
+ * Returns a matrix, that is the first Piola-Kirchoff stress tensor from
+ * - the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param sigma (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F}_0 \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the first Piola Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat sigma = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat PKI = Cauchy2PKI(sigma, F);
+ * @endcode
+*/
+arma::mat Cauchy2PKI(const arma::mat &sigma, const arma::mat &F, const double &J = 0.);
 
-//This function returns the Kirchoff stress tensor from the Cauchy stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Cauchy2Kirchoff(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the second Piola Kirchoff stress tensor \f$ \mathbf{S} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ *
+ * Returns a matrix, that is the first Piola-Kirchoff stress tensor from
+ * - the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param sigma (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the second Piola Kirchoff stress tensor \f$ \mathbf{S} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat sigma = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat PKII = Cauchy2PKII(sigma, F);
+ * @endcode
+*/
+arma::mat Cauchy2PKII(const arma::mat &sigma, const arma::mat &F, const double &J = 0.);
 
-//This function returns the Kirchoff stress tensor from the Cauchy stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::vec Cauchy2Kirchoff(const arma::vec &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the Kirchoff stress tensor \f$ \mathbf{\tau} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma}\f$
+ *
+ * Returns a matrix, that is the Kirchoff stress tensor from
+ * - the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param sigma (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat sigma = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat tau = Cauchy2Kirchoff(sigma, F);
+ * @endcode
+*/
+arma::mat Cauchy2Kirchoff(const arma::mat &sigma, const arma::mat &F, const double &J = 0.);
 
-//This function returns the Cauchy stress tensor from the Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Kirchoff2Cauchy(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the Kirchoff stress tensor \f$ \mathbf{\tau} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma}\f$
+ *
+ * Returns a vector (considering Voigt Notation), that is the Kirchoff stress tensor from
+ * - the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param sigma (arma::vec size=6) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (arma::vec size=6) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      vec sigma = m2t_stress(0.5*(matrand + matrand.t()));
+ *      mat F = eye(3,3);
+ *      vec tau = Cauchy2Kirchoff(sigma, F);
+ * @endcode
+*/
+arma::vec Cauchy2Kirchoff(const arma::vec &sigma, const arma::mat &F, const double &J = 0.);
 
-//This function returns the Cauchy stress tensor from the Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::vec Kirchoff2Cauchy(const arma::vec &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the Cauchy stress tensor \f$ \mathbf{\sigma}\f$ from the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ *
+ * Returns a matrix, that is the Cauchy stress tensor from
+ * - the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param tau (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat tau = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat sigma = Kirchoff2Cauchy(tau, F);
+ * @endcode
+*/
+arma::mat Kirchoff2Cauchy(const arma::mat &tau, const arma::mat &F, const double &J = 0.);
 
-//This function returns the second Piola-Kirchoff stress tensor from the Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Kirchoff2PKI(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the Cauchy stress tensor \f$ \mathbf{\sigma}\f$ from the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ *
+ * Returns a vecto, that is the Cauchy stress tensor from
+ * - the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param tau (arma::vec size=6) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (arma::vec size=6) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      vec tau = m2t_stress(0.5*(matrand + matrand.t()));
+ *      mat F = eye(3,3);
+ *      mat sigma = Kirchoff2Cauchy(tau, F);
+ * @endcode
+*/
+arma::vec Kirchoff2Cauchy(const arma::vec &tau, const arma::mat &F, const double &J = 0.);
 
-//This function returns the second Piola-Kirchoff stress tensor from the Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat Kirchoff2PKII(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma}\f$
+ *
+ * Returns a matrix, that is the first Piola-Kirchoff stress tensor from
+ * - the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param tau (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat tau = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat PKI = Kirchoff2PKI(tau, F);
+ * @endcode
+*/
+arma::mat Kirchoff2PKI(const arma::mat &tau, const arma::mat &F, const double &J = 0.);
 
-//This function returns the second Piola-Kirchoff stress tensor from the Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::vec Kirchoff2PKII(const arma::vec &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma}\f$
+ *
+ * Returns a matrix, that is the second Piola-Kirchoff stress tensor from
+ * - the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ \mathbf{J} \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ *
+ * @param tau (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \mathbf{F} \f$
+ * @return (3x3 arma::mat) the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ *
+ * @details Example:
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat tau = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat PKII = Kirchoff2PKII(tau, F);
+ * @endcode
+*/
+arma::mat Kirchoff2PKII(const arma::mat &tau, const arma::mat &F, const double &J = 0.);
 
-//This function returns the Kirchoff stress tensor from the second Piola-Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
-arma::mat PKI2Kirchoff(const arma::mat &, const arma::mat &, const double & = 0.);
+/**
+ * @brief Provides the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$ from the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * 
+ * Returns a vector, that is the second Piola-Kirchoff stress tensor from
+ * - the Kirchoff stress tensor \f$ \mathbf{\tau} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ J \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ * 
+ * @param tau (arma::vec size=6)) the Kirchoff stress tensor \f$ \mathbf{\tau} \f$ 
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \textrm{det}\,\mathbf{F} \f$ 
+ * @return (arma::vec size=6) the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * 
+ * @details Example: 
+ * @code
+ *      mat matrand = randu(3,3);
+ *      vec tau = m2t_stress(0.5*(matrand + matrand.t()));
+ *      mat F = eye(3,3);
+ *      vec PKII = Kirchoff2PKII(tau, F);
+ * @endcode
+*/
+arma::vec Kirchoff2PKII(const arma::vec &tau, const arma::mat &F, const double &J= 0.);
 
-//This function returns the Kirchoff stress tensor from the first Piola-Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
+/**
+ * @brief Provides the Kirchoff stress tensor \f$ \mathbf{\tau} \f$ from the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * 
+ * Returns a matrix, that is the Kirchoff stress tensor from
+ * - the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ J \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ * 
+ * @param Sigma (3x3 arma::mat) first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \textrm{det}\,\mathbf{F} \f$ 
+ * @return (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau}
+ * 
+ * @details Example: 
+ * @code
+ *      mat PKI = randu(3,3);
+ *      mat F = eye(3,3);
+ *      mat tau = PKI2Kirchoff(PKI, F);
+ * @endcode
+*/
+arma::mat PKI2Kirchoff(const arma::mat &S, const arma::mat &F, const double &J = 0.);
+
+/**
+ * @brief Provides the Kirchoff stress tensor \f$ \mathbf{\tau} \f$ from the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * 
+ * Returns a matrix, that is the Kirchoff stress tensor from
+ * - the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ J \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ * 
+ * @param S (3x3 arma::mat) the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \textrm{det}\,\mathbf{F} \f$ 
+ * @return (3x3 arma::mat) the Kirchoff stress tensor \f$ \mathbf{\tau}
+ * 
+ * @details Example: 
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat PKII = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat tau = PKI2Kirchoff(PKII, F);
+ * @endcode
+*/
 arma::mat PKII2Kirchoff(const arma::mat &, const arma::mat &, const double & = 0.);
 
-//This function returns the Cauchy stress tensor from the first Piola-Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
+
+/**
+ * @brief Provides the Cauchy stress tensor \f$ \mathbf{\sigma} \f$ from the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * 
+ * Returns a matrix, that is the Cauchy stress tensor from
+ * - the first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ J \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ * 
+ * @param Sigma (3x3 arma::mat) first Piola-Kirchoff stress tensor \f$ \mathbf{\Sigma} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \textrm{det}\,\mathbf{F} \f$ 
+ * @return (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * 
+ * @details Example: 
+ * @code
+ *      mat PKI = randu(3,3);
+ *      mat F = eye(3,3);
+ *      mat sigma = PKI2Cauchy(PKI, F);
+ * @endcode
+*/
 arma::mat PKI2Cauchy(const arma::mat &, const arma::mat &, const double & = 0.);
 
-//This function returns the Cauchy stress tensor from the second Piola-Kirchoff stress tensor, the transformation gradient F and its determinant (optional, if not indicated, it will be computed)
+/**
+ * @brief Provides the Cauchy stress tensor \f$ \mathbf{\sigma} \f$ from the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * 
+ * Returns a matrix, that is the Cauchy stress tensor from
+ * - the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * - the transformation gradient \f$ \mathbf{F} \f$ and its determinant \f$ J \f$ (this last one is optional, if not indicated, it will be computed from \f$ \mathbf{F} \f$)
+ * 
+ * @param S (3x3 arma::mat) the second Piola-Kirchoff stress tensor \f$ \mathbf{S} \f$
+ * @param F (3x3 arma::mat) transformation gradient \f$ \mathbf{F} \f$
+ * @param J (double, optional) determinant of the transformation gradient \f$ \textrm{det}\,\mathbf{F} \f$ 
+ * @return (3x3 arma::mat) the Cauchy stress tensor \f$ \mathbf{\sigma} \f$
+ * 
+ * @details Example: 
+ * @code
+ *      mat matrand = randu(3,3);
+ *      mat PKII = 0.5*(matrand + matrand.t());
+ *      mat F = eye(3,3);
+ *      mat sigma = PKII2Cauchy(PKII, F);
+ * @endcode
+*/
 arma::mat PKII2Cauchy(const arma::mat &, const arma::mat &, const double & = 0.);
     
 } //namespace simcoon
