fb9699ffa5a5b978206f2a3f225ff6416e185fd3
Yves Chemisky <yves.chemisky@gmail.com>
2023-03-07 11:38:29 +0100

    [bugfix] boost::dll::import_symbol has replaced boost::dll::import in Boost >= 1.76.0

--- src/Continuum_mechanics/Umat/umat_smart.cpp
+++ src/Continuum_mechanics/Umat/umat_smart.cpp
@@ -27,6 +27,7 @@
 #include <math.h>
 #include <vector>
 #include <armadillo>
+#include <boost/version.hpp>
 #include <boost/dll/import.hpp> // for import_alias
 
 #include <simcoon/parameter.hpp>
@@ -81,7 +82,13 @@
 
 using namespace std;
 using namespace arma;
-namespace dll = boost::dll;
+//namespace dll = boost::dll;
+
+#if BOOST_VERSION >= 107600 // 1.76.0
+#define boost_dll_import boost::dll::import_symbol
+#else
+#define boost_dll_import boost::dll::import
+#endif
 
 namespace simcoon{
 
@@ -961,10 +968,10 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
 
                 boost::filesystem::path lib_path("external");          // argv[1] contains path to directory with our plugin library
                 boost::shared_ptr<umat_plugin_ext_api> external_umat;            // variable to hold a pointer to plugin variable
-                external_umat = dll::import<umat_plugin_ext_api>(          // type of imported symbol is located between `<` and `>`
+                external_umat = boost_dll_import<umat_plugin_ext_api>(          // type of imported symbol is located between `<` and `>`
                     lib_path / "umat_plugin_ext",                     // path to the library and library name
                     "external_umat",                                       // name of the symbol to import
-                dll::load_mode::append_decorations              // makes `libmy_plugin_sum.so` or `my_plugin_sum.dll` from `my_plugin_sum`
+                boost::dll::load_mode::append_decorations              // makes `libmy_plugin_sum.so` or `my_plugin_sum.dll` from `my_plugin_sum`
                 );
                 
                 external_umat->umat_external_M(umat_M->Etot, umat_M->DEtot, umat_M->sigma, umat_M->Lt, umat_M->L, umat_M->sigma_in, DR, rve.sptr_matprops->nprops, rve.sptr_matprops->props, umat_M->nstatev, umat_M->statev, umat_M->T, umat_M->DT, Time, DTime, umat_M->Wm(0), umat_M->Wm(1), umat_M->Wm(2), umat_M->Wm(3), ndi, nshr, start, solver_type, tnew_dt);
@@ -976,7 +983,7 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
                 boost::filesystem::path lib_path("external");
                 boost::shared_ptr<umat_plugin_aba_api> abaqus_umat;            // variable to hold a pointer to plugin variable
 
-                abaqus_umat = dll::import<umat_plugin_aba_api>(lib_path / "umat_plugin_aba", "abaqus_umat", dll::load_mode::append_decorations);
+                abaqus_umat = boost_dll_import<umat_plugin_aba_api>(lib_path / "umat_plugin_aba", "abaqus_umat", boost::dll::load_mode::append_decorations);
                 abaqus_umat->umat_abaqus(rve, DR, Time, DTime, ndi, nshr, start, solver_type, tnew_dt);
                 break;
             }
