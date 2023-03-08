a31fa3aa7980364811639e6e11241fa608e87d08
pruliere <etienne.pruliere@ensam.eu>
2023-03-08 10:53:39 +0100

    Change cmake files and Install.bat for windows installation including libopenblas

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
-:: conda install -c conda-forge git cmake python=3.9 cgal boost=1.72 -y
+:: conda install -c conda-forge git cmake python=3.9 cgal boost -y
 
 set PREFIX=%CONDA_PREFIX%
 set SRC_DIR=%cd%
@@ -7,10 +7,14 @@ set SP_DIR=%PREFIX%\Lib\site-packages
 :: Armadillo
 git clone https://gitlab.com/conradsnicta/armadillo-code.git armadillo
 cd armadillo
-cmake -S . -B build -G "Visual Studio 17 2022"
+copy /y .\examples\lib_win64\libopenblas.dll %CONDA_PREFIX%\Library\bin\
+copy /y .\examples\lib_win64\libopenblas.lib %CONDA_PREFIX%\Library\lib\
+cmake -S . -B build -G "Visual Studio 17 2022" -DBLAS_LIBRARY:FILEPATH="%CONDA_PREFIX%\Library\lib\libopenblas.lib" -DLAPACK_LIBRARY:FILEPATH="%CONDA_PREFIX%\Library\lib\libopenblas.lib"
 cmake --build build --config RELEASE
+cmake --install build --prefix %CONDA_PREFIX%\Library
 if errorlevel 1 exit 1
 
+:: Simcoon
 cd %SRC_DIR%
 cmake -S . -B build ^
       -G"Visual Studio 17 2022" ^
@@ -21,6 +25,7 @@ cmake --build build --target simcoon --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
+:: Arma2numpy
 cd arma2numpy-builder
 cmake -S . -B build ^
       -G "Visual Studio 17 2022" ^
@@ -32,6 +37,7 @@ cmake --build build --target ALL_BUILD --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
+:: Python binding
 cd %SRC_DIR%\simcoon-python-builder
 cmake -S . -B build ^
       -G "Visual Studio 17 2022" ^
@@ -44,13 +50,15 @@ cmake --install build
 if errorlevel 1 exit 1
 
 cd %SP_DIR%
+
+:: Install simcoon python 
 mkdir simcoon
 cd simcoon
 type NUL > __init__.py
 xcopy /s /i %SRC_DIR%\simcoon-python-builder\build\lib\Release\simmit.pyd %SP_DIR%\simcoon
 if errorlevel 1 exit 1
 
-xcopy /s /i %SRC_DIR%\armadillo\examples\lib_win64\libopenblas.dll %PREFIX%\Library\bin
+REM xcopy /s /i %SRC_DIR%\armadillo\examples\lib_win64\libopenblas.dll %PREFIX%\Library\bin
 xcopy /s /i %SRC_DIR%\build\lib\Release\simcoon.dll %PREFIX%\Library\bin
 xcopy /s /i %SRC_DIR%\arma2numpy-builder\build\bin\Release\arma2numpy.dll %PREFIX%\Library\bin
 if errorlevel 1 exit 1
