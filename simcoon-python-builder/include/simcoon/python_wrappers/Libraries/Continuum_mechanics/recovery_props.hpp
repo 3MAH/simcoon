1c945961e117211067ae2486760e3f6066f7912b
Yves Chemisky <yves.chemisky@gmail.com>
2023-03-22 16:25:19 +0100

    transfer bindings boost::numpy to pybind11 for recovery_props

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/recovery_props.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/recovery_props.hpp
@@ -1,37 +1,37 @@
 #pragma once
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 namespace simpy{
 
 //Check the material symetries and the type of elastic response for a given stiffness tensor
-boost::python::dict check_symetries(const boost::python::numpy::ndarray &);
+pybind11::dict check_symetries(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the isotropic case (E,nu) from a stiffness tensor
-boost::python::numpy::ndarray L_iso_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the isotropic case (E,nu) from a stiffness tensor
+pybind11::array_t<double> L_iso_props(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the isotropic case (E,nu) from a compliance tensor
-boost::python::numpy::ndarray M_iso_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the isotropic case (E,nu) from a compliance tensor
+pybind11::array_t<double> M_iso_props(const pybind11::array_t<double> &);
     
-//return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a stiffness tensor
-boost::python::numpy::ndarray L_isotrans_props(const boost::python::numpy::ndarray &, const int &);
+//Return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a stiffness tensor
+pybind11::array_t<double> L_isotrans_props(const pybind11::array_t<double> &, const int &);
 
-//return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a compliance tensor
-boost::python::numpy::ndarray M_isotrans_props(const boost::python::numpy::ndarray &, const int &);
+//Return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a compliance tensor
+pybind11::array_t<double> M_isotrans_props(const pybind11::array_t<double> &, const int &);
 
-//return a list of elastic properties for the cubic case (E,nu,G) from a stiffness tensor
-boost::python::numpy::ndarray L_cubic_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the cubic case (E,nu,G) from a stiffness tensor
+pybind11::array_t<double> L_cubic_props(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the cubic case (E,nu,G) from a compliance tensor
-boost::python::numpy::ndarray M_cubic_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the cubic case (E,nu,G) from a compliance tensor
+pybind11::array_t<double> M_cubic_props(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a stiffness tensor
-boost::python::numpy::ndarray L_ortho_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a stiffness tensor
+pybind11::array_t<double> L_ortho_props(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a compliance tensor
-boost::python::numpy::ndarray M_ortho_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a compliance tensor
+pybind11::array_t<double> M_ortho_props(const pybind11::array_t<double> &);
 
-//return a list of elastic properties for the anisotropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23,deviations) from a compliance tensor
-boost::python::numpy::ndarray M_aniso_props(const boost::python::numpy::ndarray &);
+//Return a list of elastic properties for the anisotropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23,deviations) from a compliance tensor
+pybind11::array_t<double> M_aniso_props(const pybind11::array_t<double> &);
     
 } //namespace simpy
--- simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/recovery_props.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/recovery_props.cpp
@@ -1,103 +1,100 @@
 
+#include <string>
+#include <carma>
 #include <armadillo>
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
-#include <simcoon/arma2numpy/numpy_arma.hpp>
 
 #include <simcoon/Continuum_mechanics/Functions/recovery_props.hpp>
 #include <simcoon/python_wrappers/Libraries/Continuum_mechanics/recovery_props.hpp>
 
-namespace bp = boost::python;
-namespace bn = boost::python::numpy;
 using namespace std;
 using namespace arma;
-using namespace arma2numpy;
+namespace py=pybind11;
 
 namespace simpy {
 
 //Check the material symetries and the type of elastic response for a given stiffness tensor
-bp::dict check_symetries(const bn::ndarray &nL) {
+py::dict check_symetries(const py::array_t<double> &nL) {
     
-    mat L = array2mat(nL);
+    mat L = carma::arr_to_mat(nL);
     int axis = 0;
     string umat_type;
     vec props;
     int maj_sym = 0;
     simcoon::check_symetries(L, umat_type, axis, props, maj_sym);
-    bp::dict d;
+    py::dict d;
     d["umat_type"]  = umat_type;
     d["axis"]  = axis;
     d["maj_sym"]  = maj_sym;
-    d["props"]  = vec2array(props);
+    d["props"]  = carma::col_to_arr(props);
     return d;
 }
 
 //return a list of elastic properties for the isotropic case (E,nu) from a stiffness tensor
-bn::ndarray L_iso_props(const bn::ndarray &nLt) {
+py::array_t<double> L_iso_props(const py::array_t<double> &nLt) {
 
-    mat Lt = array2mat(nLt);
+    mat Lt = carma::arr_to_mat(nLt);
     vec props = simcoon::L_iso_props(Lt);
-    return vec2array(props);
+    return carma::col_to_arr(props);
 }
     
 //return a list of elastic properties for the isotropic case (E,nu) from a compliance tensor
-bn::ndarray M_iso_props(const bn::ndarray &nMt) {
+py::array_t<double> M_iso_props(const py::array_t<double> &nMt) {
     
-    mat Mt = array2mat(nMt);
+    mat Mt = carma::arr_to_mat(nMt);
     vec props = simcoon::M_iso_props(Mt);
     return vec2array(props);
 }
 
 //return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a stiffness tensor
-bn::ndarray L_isotrans_props(const bn::ndarray &nLt, const int &axis) {
+py::array_t<double> L_isotrans_props(const py::array_t<double> &nLt, const int &axis) {
     mat Lt = array2mat(nLt);
     vec props = simcoon::L_isotrans_props(Lt, axis);
     return vec2array(props);
 }
     
 //return a list of elastic properties for the transversely isotropic case (EL,ET,nuTL,nuTT,GLT) from a compliance tensor
-bn::ndarray M_isotrans_props(const bn::ndarray &nMt, const int &axis) {
-    mat Mt = array2mat(nMt);
+py::array_t<double> M_isotrans_props(const py::array_t<double> &nMt, const int &axis) {
+    mat Mt = carma::arr_to_mat(nMt);
     vec props = simcoon::M_isotrans_props(Mt, axis);
     return vec2array(props);
 }
 
 //return a list of elastic properties for the cubic case (E,nu,G) from a stiffness tensor
-bn::ndarray L_cubic_props(const bn::ndarray &nLt) {
+py::array_t<double> L_cubic_props(const py::array_t<double> &nLt) {
     
-    mat Lt = array2mat(nLt);
+    mat Lt = carma::arr_to_mat(nLt);
     vec props = simcoon::L_cubic_props(Lt);
     return vec2array(props);
 }
     
 //return a list of elastic properties for the cubic case (E,nu,G) from a compliance tensor
-bn::ndarray M_cubic_props(const bn::ndarray &nMt) {
+py::array_t<double> M_cubic_props(const py::array_t<double> &nMt) {
     
-    mat Mt = array2mat(nMt);
+    mat Mt = carma::arr_to_mat(nMt);
     vec props = simcoon::M_cubic_props(Mt);
     return vec2array(props);
 }
  
 //return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a stiffness tensor
-bn::ndarray L_ortho_props(const bn::ndarray &nLt) {
+py::array_t<double> L_ortho_props(const py::array_t<double> &nLt) {
     
-    mat Lt = array2mat(nLt);
+    mat Lt = carma::arr_to_mat(nLt);
     vec props = simcoon::L_ortho_props(Lt);
     return vec2array(props);
 }
 
 //return a list of elastic properties for the orthtropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23) from a compliance tensor
-bn::ndarray M_ortho_props(const bn::ndarray &nMt) {
+py::array_t<double> M_ortho_props(const py::array_t<double> &nMt) {
     
-    mat Mt = array2mat(nMt);
+    mat Mt = carma::arr_to_mat(nMt);
     vec props = simcoon::M_ortho_props(Mt);
     return vec2array(props);
 }
     
 //return a list of elastic properties for the anisotropic case (E1,E2,E3,nu12,nu13,nu23,G12,G13,G23,deviations) from a compliance tensor
-bn::ndarray M_aniso_props(const bn::ndarray &nMt) {
+py::array_t<double> M_aniso_props(const py::array_t<double> &nMt) {
     
-    mat Mt = array2mat(nMt);
+    mat Mt = carma::arr_to_mat(nMt);
     vec props = simcoon::M_aniso_props(Mt);
     return vec2array(props);
 }
