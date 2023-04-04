f4f6e1ba09eebd77823cecd5657cd34209c2709e
Yves Chemisky <yves.chemisky@gmail.com>
2023-04-04 10:43:09 +0200

    Added variable names for constitutive

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/criteria.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/criteria.hpp
@@ -5,39 +5,39 @@
 namespace simpy{
     
 //This function returns the Prager equivalent stress.
-double Prager_stress(const pybind11::array_t<double> &, const double &, const double &);
+double Prager_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props);
 
 //This function returns the derivative of the Prager equivalent stress.
-pybind11::array_t<double> dPrager_stress(const pybind11::array_t<double> &, const double &, const double &, const bool &copy=true);
+pybind11::array_t<double> dPrager_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props, const bool &copy=true);
 
 //This function returns the Prager equivalent stress.
-double Tresca_stress(const pybind11::array_t<double> &);
+double Tresca_stress(const pybind11::array_t<double> &input);
 
 //This function returns the derivative of the Prager equivalent stress.
-pybind11::array_t<double> dTresca_stress(const pybind11::array_t<double> &, const bool &copy=true);
+pybind11::array_t<double> dTresca_stress(const pybind11::array_t<double> &input, const bool &copy=true);
 
 //Returns an anisotropic configurational tensor P in the Voigt format (6x6 numpy array), given its vector representation
-pybind11::array_t<double> P_ani(const pybind11::array_t<double> &P_params, const bool &copy=true);
+pybind11::array_t<double> P_ani(const pybind11::array_t<double> &props, const bool &copy=true);
 
 //Provides an anisotropic configurational tensor considering the quadratic Hill yield criterion in the Voigt format (6x6 numpy array), given its vector representation
-pybind11::array_t<double> P_hill(const pybind11::array_t<double> &P_params, const bool &copy=true);
+pybind11::array_t<double> P_hill(const pybind11::array_t<double> &props, const bool &copy=true);
 
 //This function returns the Hill equivalent stress.
-double Hill_stress(const pybind11::array_t<double> &, const pybind11::array_t<double> &);
+double Hill_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props);
 
 //This function returns the derivative of the Hill equivalent stress.
-pybind11::array_t<double> dHill_stress(const pybind11::array_t<double> &, const pybind11::array_t<double> &, const bool &copy=true);
+pybind11::array_t<double> dHill_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props, const bool &copy=true);
     
 //This function returns the Ani equivalent stress.
-double Ani_stress(const pybind11::array_t<double> &, const pybind11::array_t<double> &);
+double Ani_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props);
 
 //This function returns the derivative of the Ani equivalent stress.
-pybind11::array_t<double> dAni_stress(const pybind11::array_t<double> &, const pybind11::array_t<double> &, const bool &copy=true);
+pybind11::array_t<double> dAni_stress(const pybind11::array_t<double> &input, const pybind11::array_t<double> &props, const bool &copy=true);
     
 //This function computes the selected equivalent stress function
-double Eq_stress(const pybind11::array_t<double> &, const std::string &, const pybind11::array_t<double> &);
+double Eq_stress(const pybind11::array_t<double> &input, const std::string &criteria, const pybind11::array_t<double> props);
 
 //This function computes the deriavtive of the selected equivalent stress function
-pybind11::array_t<double> dEq_stress(const pybind11::array_t<double> &, const std::string &, const pybind11::array_t<double> &, const bool &copy=true);
+pybind11::array_t<double> dEq_stress(const pybind11::array_t<double> &input, const std::string &criteria, const pybind11::array_t<double> &props, const bool &copy=true);
     
 } //namespace simpy
--- simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/criteria.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/criteria.cpp
@@ -15,57 +15,89 @@ namespace py=pybind11;
 namespace simpy {
 
 //This function returns the Prager equivalent stress.
-double Prager_stress(const py::array_t<double> &nd, const double &b, const double &n) {
-    vec v = carma::arr_to_col(nd);
-    return simcoon::Prager_stress(v,b,n);
+double Prager_stress(const py::array_t<double> &input, const py::array_t<double> &props) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);    
+    return simcoon::Prager_stress(v,props_cpp(0),props_cpp(1));
 }
 
 //This function returns the derivative of the Prager equivalent stress.
-py::array_t<double> dPrager_stress(const py::array_t<double> &nd, const double &b, const double &n, const bool &copy) {
-    vec v = carma::arr_to_col(nd);
-    vec t = simcoon::dPrager_stress(v,b,n);
+py::array_t<double> dPrager_stress(const py::array_t<double> &input, const py::array_t<double> &props, const bool &copy) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);
+    vec t = simcoon::dPrager_stress(v,props_cpp(0),props_cpp(1));
     return carma::col_to_arr(t, copy);
 }
 
 //This function returns the Tresca equivalent stress.
-double Tresca_stress(const py::array_t<double> &nd) {
-    vec v = carma::arr_to_col(nd);
+double Tresca_stress(const py::array_t<double> &input) {
+    vec v = carma::arr_to_col(input);
     return simcoon::Tresca_stress(v);
 }
 
 //This function returns the derivative of the Tresca equivalent stress.
-py::array_t<double> dTresca_stress(const py::array_t<double> &nd, const bool &copy) {
-    vec v = carma::arr_to_col(nd);
+py::array_t<double> dTresca_stress(const py::array_t<double> &input, const bool &copy) {
+    vec v = carma::arr_to_col(input);
     vec t = simcoon::dTresca_stress(v);
     return carma::col_to_arr(t, copy);
 }
 
 //Provides an anisotropic configurational tensor P in the Voigt format (6x6 numpy array), given its vector representation
-py::array_t<double> P_ani(const py::array_t<double> &nparams, const bool &copy) {
-    mat m = carma::arr_to_mat(nparams);
-    mat t = simcoon::P_ani(m);
+py::array_t<double> P_ani(const py::array_t<double> &props, const bool &copy) {
+    vec v = carma::arr_to_col(props);
+    mat t = simcoon::P_ani(v);
     return carma::mat_to_arr(t, copy);
 }
 
 //Provides an anisotropic configurational tensor considering the quadratic Hill yield criterion in the Voigt format (6x6 numpy array), given its vector representation
-py::array_t<double> P_hill(const py::array_t<double> &nparams, const bool &copy) {
-    mat m = carma::arr_to_mat(nparams);
-    mat t = simcoon::P_hill(m);
+py::array_t<double> P_hill(const py::array_t<double> &props, const bool &copy) {
+    vec v = carma::arr_to_col(props);
+    mat t = simcoon::P_hill(v);
     return carma::mat_to_arr(t, copy);
 }
 
+//This function returns the Tresca equivalent stress.
+double Hill_stress(const py::array_t<double> &input, const py::array_t<double> &props) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);
+    return simcoon::Hill_stress(v, props_cpp);
+}
+
+//This function returns the derivative of the Tresca equivalent stress.
+py::array_t<double> dHill_stress(const py::array_t<double> &input, const py::array_t<double> &props, const bool &copy) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);    
+    vec t = simcoon::dHill_stress(v, props_cpp);
+    return carma::col_to_arr(t, copy);
+}
+
+//This function returns the Tresca equivalent stress.
+double Ani_stress(const py::array_t<double> &input, const py::array_t<double> &props) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);    
+    return simcoon::Ani_stress(v, props_cpp);
+}
+
+//This function returns the derivative of the Tresca equivalent stress.
+py::array_t<double> dAni_stress(const py::array_t<double> &input, const py::array_t<double> &props, const bool &copy) {
+    vec v = carma::arr_to_col(input);
+    vec props_cpp = carma::arr_to_col(props);      
+    vec t = simcoon::dAni_stress(v, props_cpp);
+    return carma::col_to_arr(t, copy);
+}
+
 //This function computes the selected equivalent stress function
-double Eq_stress(const py::array_t<double> &nd, const string &eq_type, const py::array_t<double> &ndparam) {
-    vec v = carma::arr_to_col(nd);
+double Eq_stress(const py::array_t<double> &input, const string &criteria, const py::array_t<double> &ndparam) {
+    vec v = carma::arr_to_col(input);
     vec param = carma::arr_to_col(ndparam);
-    return simcoon::Eq_stress(v,eq_type,param);
+    return simcoon::Eq_stress(v,criteria,param);
 }
 
 //This function computes the deriavtive of the selected equivalent stress function
-py::array_t<double> dEq_stress(const py::array_t<double> &nd, const string &eq_type, const py::array_t<double> &ndparam, const bool &copy) {
-    vec v = carma::arr_to_col(nd);
+py::array_t<double> dEq_stress(const py::array_t<double> &input, const string &criteria, const py::array_t<double> &ndparam, const bool &copy) {
+    vec v = carma::arr_to_col(input);
     vec param = carma::arr_to_col(ndparam);
-    vec t = simcoon::dEq_stress(v,eq_type,param);
+    vec t = simcoon::dEq_stress(v,criteria,param);
     return carma::col_to_arr(t, copy);
 }
 
