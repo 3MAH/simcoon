162df166479658a285b1b6f6f5636f89c034d602
Yves Chemisky <yves.chemisky@gmail.com>
2023-04-04 10:43:23 +0200

    Switch to pybind11

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/kinematics.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/kinematics.hpp
@@ -1,50 +1,62 @@
 #pragma once
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 namespace simpy{
-    
+
+//Provides the transformation gradient, from the Green-Lagrange strain and the rotation.   
+pybind11::array_t<double> ER_to_F(const pybind11::array_t<double> &E, pybind11::array_t<double> &R, const bool &copy=true);
+
+//Provides the transformation gradient, from the logarithmic strain and the rotation.
+pybind11::array_t<double> eR_to_F(const pybind11::array_t<double> &e, const pybind11::array_t<double> &R, const bool &copy=true);
+
 //This function computes the gradient of displacement (Lagrangian) from the deformation gradient tensor
-boost::python::numpy::ndarray G_UdX(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> G_UdX(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the gradient of displacement (Eulerian) from the deformation gradient tensor
-boost::python::numpy::ndarray G_Udx(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> G_Udx(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the Right Cauchy-Green C
-boost::python::numpy::ndarray R_Cauchy_Green(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> R_Cauchy_Green(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the Left Cauchy-Green B
-boost::python::numpy::ndarray L_Cauchy_Green(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> L_Cauchy_Green(const pybind11::array_t<double> &F, const bool &copy=true);
+
+//Provides the RU decomposition of the transformation gradient F
+pybind11::tuple RU_decomposition(const pybind11::array_t<double> &F, const bool &copy=true);
+
+//Provides the VR decomposition of the transformation gradient F
+pybind11::tuple VR_decomposition(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the common Right (or Left) Cauchy-Green invariants
-boost::python::numpy::ndarray Inv_X(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> Inv_X(const pybind11::array_t<double> &input, const bool &copy=true);
 
-//This function computes the Cauchy deformation tensor c
-boost::python::numpy::ndarray Cauchy(const boost::python::numpy::ndarray &);
+//This function computes the Cauchy deformation tensor c from the transformation gradient F
+pybind11::array_t<double> Cauchy(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the Green-Lagrange finite strain tensor E
-boost::python::numpy::ndarray Green_Lagrange(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> Green_Lagrange(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the Euler-Almansi finite strain tensor A
-boost::python::numpy::ndarray Euler_Almansi(const boost::python::numpy::ndarray &);
+pybind11::array_t<double> Euler_Almansi(const pybind11::array_t<double> &F, const bool &copy=true);
 
-//This function computes the logarithmic strain ln[V] = 1/2 ln[b] (b is the left Cauchy-Green Tensor)
-boost::python::numpy::ndarray Log_strain(const boost::python::numpy::ndarray &);
+//This function computes the logarithmic strain ln[V] = 1/2 ln[B] (B is the left Cauchy-Green Tensor)
+pybind11::array_t<double> Log_strain(const pybind11::array_t<double> &F, const bool &copy=true);
 
 //This function computes the velocity difference (F0,F1,DTime)
-boost::python::numpy::ndarray finite_L(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> finite_L(const pybind11::array_t<double> &F0, const pybind11::array_t<double> &F1, const double &DTime, const bool &copy=true);
 
 //This function computes the deformation rate D (F0,F1,DTime)
-boost::python::numpy::ndarray finite_D(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> finite_D(const pybind11::array_t<double> &F0, const pybind11::array_t<double> &F1, const double &DTime, const bool &copy=true);
 
 //This function computes the spin tensor W (correspond to Jaumann rate) (F0,F1,DTime)
-boost::python::numpy::ndarray finite_W(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> finite_W(const pybind11::array_t<double> &F0, const pybind11::array_t<double> &F1, const double &DTime, const bool &copy=true);
 
 //This function computes the spin tensor Omega (corrspond to Green-Naghdi rate)
 // Note : here R is the is the rigid body rotation in the RU or VR polar decomposition of the deformation gradient F (F0,F1,DTime)
-boost::python::numpy::ndarray finite_Omega(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> finite_Omega(const pybind11::array_t<double> &F0, const pybind11::array_t<double> &F1, const double &DTime, const bool &copy=true);
 
 //This function computes the increment of finite rotation (Omega0, Omega1, DTime)
-boost::python::numpy::ndarray finite_DQ(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> finite_DQ(const pybind11::array_t<double> &Omega0, const pybind11::array_t<double> &Omega1, const double &DTime, const bool &copy=true);
     
 } //namespace simpy
--- simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/kinematics.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/kinematics.cpp
@@ -1,107 +1,156 @@
 
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include <string>
+#include <carma>
 #include <armadillo>
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
-#include <simcoon/arma2numpy/numpy_arma.hpp>
 
 #include <simcoon/Continuum_mechanics/Functions/kinematics.hpp>
 #include <simcoon/python_wrappers/Libraries/Continuum_mechanics/kinematics.hpp>
 
-namespace bn = boost::python::numpy;
 using namespace std;
 using namespace arma;
-using namespace arma2numpy;
+namespace py=pybind11;
 
 namespace simpy {
 
+//Provides the transformation gradient, from the Green-Lagrange strain and the rotation. 
+py::array_t<double> ER_to_F(const py::array_t<double> &E, const py::array_t<double> &R, const bool &copy) {
+    mat E_cpp = carma::arr_to_mat(E);
+    mat R_cpp = carma::arr_to_mat(R);
+    mat F = simcoon::ER_to_F(E_cpp, R_cpp);    
+    return carma::mat_to_arr(F, copy);
+}
+
+//Provides the transformation gradient, from the Green-Lagrange strain and the rotation. 
+py::array_t<double> eR_to_F(const py::array_t<double> &e, const py::array_t<double> &R, const bool &copy) {
+    mat e_cpp = carma::arr_to_mat(e);
+    mat R_cpp = carma::arr_to_mat(R);
+    mat F = simcoon::eR_to_F(e_cpp, R_cpp);    
+    return carma::mat_to_arr(F, copy);
+}
+
 //This function computes the gradient of displacement (Lagrangian) from the deformation gradient tensor
-bn::ndarray G_UdX(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::G_UdX(F));
+py::array_t<double> G_UdX(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat G_UdX = simcoon::G_UdX(F_cpp);
+    return carma::mat_to_arr(G_UdX, copy);
 }
 
 //This function computes the gradient of displacement (Eulerian) from the deformation gradient tensor
-bn::ndarray G_Udx(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::G_Udx(F));
+py::array_t<double> G_Udx(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat G_Udx = simcoon::G_Udx(F_cpp);
+    return carma::mat_to_arr(G_Udx, copy);
 }
 
 //This function computes the Right Cauchy-Green C
-bn::ndarray R_Cauchy_Green(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::R_Cauchy_Green(F));
+py::array_t<double> R_Cauchy_Green(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat R_Cauchy_Green = simcoon::R_Cauchy_Green(F_cpp);
+    return carma::mat_to_arr(R_Cauchy_Green, copy);
 }
 
 //This function computes the Left Cauchy-Green B
-bn::ndarray L_Cauchy_Green(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::L_Cauchy_Green(F));
+py::array_t<double> L_Cauchy_Green(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat L_Cauchy_Green = simcoon::L_Cauchy_Green(F_cpp);
+    return carma::mat_to_arr(L_Cauchy_Green, copy);
+}
+
+//Provides the RU decomposition of the transformation gradient F
+py::tuple RU_decomposition(const pybind11::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat R_cpp = zeros(3,3);
+    mat U_cpp = zeros(3,3);
+    simcoon::RU_decomposition(R_cpp,U_cpp,F_cpp);
+    return py::make_tuple(carma::mat_to_arr(R_cpp, copy), carma::mat_to_arr(U_cpp, copy));
+}
+
+//Provides the VR decomposition of the transformation gradient F
+py::tuple VR_decomposition(const pybind11::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat V_cpp = zeros(3,3);
+    mat R_cpp = zeros(3,3);    
+    simcoon::VR_decomposition(V_cpp,R_cpp,F_cpp);
+    return py::make_tuple(carma::mat_to_arr(V_cpp, copy), carma::mat_to_arr(R_cpp, copy));
 }
 
 //This function computes the common Right (or Left) Cauchy-Green invariants
-bn::ndarray Inv_X(const bn::ndarray &nd) {
-    mat X = array2mat(nd);
-    return vec2array(simcoon::Inv_X(X));
+py::array_t<double> Inv_X(const py::array_t<double> &input, const bool &copy) {
+    mat X = carma::arr_to_mat(input);
+    vec Inv_X = simcoon::Inv_X(X);
+    return carma::col_to_arr(Inv_X, copy);
 }
 
 //This function computes the Cauchy deformation tensor c
-bn::ndarray Cauchy(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::Cauchy(F));
+py::array_t<double> Cauchy(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat Cauchy = simcoon::Cauchy(F_cpp);
+    return carma::mat_to_arr(Cauchy, copy);
 }
 
 //This function computes the Green-Lagrange finite strain tensor E
-bn::ndarray Green_Lagrange(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::Green_Lagrange(F));
+py::array_t<double> Green_Lagrange(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat Green_Lagrange = simcoon::Green_Lagrange(F_cpp);
+    return carma::mat_to_arr(Green_Lagrange, copy);
 }
 
 //This function computes the Euler-Almansi finite strain tensor A
-bn::ndarray Euler_Almansi(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::Euler_Almansi(F));
+py::array_t<double> Euler_Almansi(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat Euler_Almansi = simcoon::Euler_Almansi(F_cpp);
+    return carma::mat_to_arr(Euler_Almansi, copy);
 }
 
 //This function computes the logarithmic strain ln[V] = 1/2 ln[b] (b is the left Cauchy-Green Tensor)
-bn::ndarray Log_strain(const bn::ndarray &nd) {
-    mat F = array2mat(nd);
-    return mat2array(simcoon::Log_strain(F));
+py::array_t<double> Log_strain(const py::array_t<double> &F, const bool &copy) {
+    mat F_cpp = carma::arr_to_mat(F);
+    mat Log_strain = simcoon::Log_strain(F_cpp);
+    return carma::mat_to_arr(Log_strain, copy);
 }
 
 //This function computes the velocity difference
-bn::ndarray finite_L(const bn::ndarray &ndF, const bn::ndarray &ndDF, const double &DTime) {
-    mat F = array2mat(ndF);
-    mat DF = array2mat(ndDF);
-    return mat2array(simcoon::finite_L(F,DF,DTime));
+py::array_t<double> finite_L(const py::array_t<double> &F0, const py::array_t<double> &F1, const double &DTime, const bool &copy) {
+    mat F0_cpp = carma::arr_to_mat(F0);
+    mat F1_cpp = carma::arr_to_mat(F1);
+    mat finite_L = simcoon::finite_L(F0_cpp,F1_cpp,DTime);
+    return carma::mat_to_arr(finite_L, copy);
 }
 
 //This function computes the deformation rate D
-bn::ndarray finite_D(const bn::ndarray &ndF0, const bn::ndarray &ndF1, const double &DTime) {
-    mat F0 = array2mat(ndF0);
-    mat F1 = array2mat(ndF1);
-    return mat2array(simcoon::finite_D(F0,F1,DTime));
+py::array_t<double> finite_D(const py::array_t<double> &F0, const py::array_t<double> &F1, const double &DTime, const bool &copy) {
+    mat F0_cpp = carma::arr_to_mat(F0);
+    mat F1_cpp = carma::arr_to_mat(F1);
+    mat finite_D = simcoon::finite_D(F0_cpp,F1_cpp,DTime);
+    return carma::mat_to_arr(finite_D, copy);
 }
 
 //This function computes the spin tensor W (correspond to Jaumann rate)
-bn::ndarray finite_W(const bn::ndarray &ndF0, const bn::ndarray &ndF1, const double &DTime) {
-    mat F0 = array2mat(ndF0);
-    mat F1 = array2mat(ndF1);
-    return mat2array(simcoon::finite_W(F0,F1,DTime));
+py::array_t<double> finite_W(const py::array_t<double> &F0, const py::array_t<double> &F1, const double &DTime, const bool &copy) {
+    mat F0_cpp = carma::arr_to_mat(F0);
+    mat F1_cpp = carma::arr_to_mat(F1);
+    mat finite_W = simcoon::finite_W(F0_cpp,F1_cpp,DTime);
+    return carma::mat_to_arr(finite_W, copy);
 }
 
 //This function computes the spin tensor Omega (corrspond to Green-Naghdi rate)
 // Note : here R is the is the rigid body rotation in the polar decomposition of the deformation gradient F
-bn::ndarray finite_Omega(const bn::ndarray &ndF0, const bn::ndarray &ndF1, const double &DTime) {
-    mat F0 = array2mat(ndF0);
-    mat F1 = array2mat(ndF1);
-    return mat2array(simcoon::finite_Omega(F0,F1,DTime));
+py::array_t<double> finite_Omega(const py::array_t<double> &F0, const py::array_t<double> &F1, const double &DTime, const bool &copy) {
+    mat F0_cpp = carma::arr_to_mat(F0);
+    mat F1_cpp = carma::arr_to_mat(F1);
+    mat finite_Omega = simcoon::finite_Omega(F0_cpp,F1_cpp,DTime);
+    return carma::mat_to_arr(finite_Omega, copy);
 }
 
 //This function computes the increment of finite rotation
-bn::ndarray finite_DQ(const bn::ndarray &ndOmega0, const bn::ndarray &ndOmega1, const double &DTime) {
-    mat Omega0 = array2mat(ndOmega0);
-    mat Omega1 = array2mat(ndOmega1);
-    return mat2array(simcoon::finite_DQ(Omega0,Omega1,DTime));
+py::array_t<double> finite_DQ(const py::array_t<double> &Omega0, const py::array_t<double> &Omega1, const double &DTime, const bool &copy) {
+    mat Omega0_cpp = carma::arr_to_mat(Omega0);
+    mat Omega1_cpp = carma::arr_to_mat(Omega1);
+    mat finite_DQ = simcoon::finite_DQ(Omega0_cpp,Omega1_cpp,DTime);
+    return carma::mat_to_arr(finite_DQ, copy);
 }
 
 } //namepsace simpy
