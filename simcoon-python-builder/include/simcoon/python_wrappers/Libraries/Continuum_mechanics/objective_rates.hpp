725f37500de9de37110ee479d0551e4ed45f43a2
Yves Chemisky <yves.chemisky@gmail.com>
2023-04-08 21:56:01 +0200

    Added objective rate functions

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/objective_rates.hpp
+++ simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Continuum_mechanics/objective_rates.hpp
@@ -1,12 +1,12 @@
 #pragma once
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 namespace simpy{
     
 //This function computes the gradient of displacement (Lagrangian) from the deformation gradient tensor
-boost::python::tuple logarithmic(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::tuple logarithmic(const pybind11::array_t<double> &F0, const pybind11::array_t<double> &F1, const double &DTime, const bool &copy=true);
     
-boost::python::numpy::ndarray Delta_log_strain(const boost::python::numpy::ndarray &, const boost::python::numpy::ndarray &, const double &);
+pybind11::array_t<double> Delta_log_strain(const pybind11::array_t<double> &D, const pybind11::array_t<double> &Omega, const double &DTime, const bool &copy=true);
 
 } //namespace simpy
--- simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/objective_rates.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Continuum_mechanics/objective_rates.cpp
@@ -1,39 +1,37 @@
 
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include <string>
+#include <carma>
 #include <armadillo>
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
-#include <simcoon/arma2numpy/numpy_arma.hpp>
 
 #include <simcoon/Continuum_mechanics/Functions/objective_rates.hpp>
 #include <simcoon/python_wrappers/Libraries/Continuum_mechanics/objective_rates.hpp>
 
-namespace bp = boost::python;
-namespace bn = boost::python::numpy;
 using namespace std;
 using namespace arma;
-using namespace arma2numpy;
+namespace py=pybind11;
 
 namespace simpy {
 
 //This function computes the logarithmic strain velocity and the logarithmic spin, along with the correct rotation increment
-bp::tuple logarithmic(const bn::ndarray &nF0, const bn::ndarray &nF1, const double &DTime) {
-    mat F0 = array2mat(nF0);
-    mat F1 = array2mat(nF1);
+py::tuple logarithmic(const py::array_t<double> &F0, const py::array_t<double> &F1, const double &DTime, const bool &copy) {
+    mat F0_cpp = carma::arr_to_mat(F0);
+    mat F1_cpp = carma::arr_to_mat(F1);
     mat DR = zeros(3,3);
     mat D = zeros(3,3);
     mat Omega = zeros(3,3);
-    simcoon::logarithmic(DR, D, Omega, DTime, F0, F1);
-    bn::ndarray nD = mat2array(D);
-    bn::ndarray nDR = mat2array(DR);
-    bn::ndarray nOmega = mat2array(Omega);
-    return boost::python::make_tuple(nD, nDR, nOmega);
+    simcoon::logarithmic(DR, D, Omega, DTime, F0_cpp, F1_cpp);
+    return py::make_tuple(carma::mat_to_arr(D, copy), carma::mat_to_arr(DR, copy), carma::mat_to_arr(Omega, copy));
 }
 
 //This function computes the gradient of displacement (Eulerian) from the deformation gradient tensor
-bn::ndarray Delta_log_strain(const bn::ndarray &nD, const bn::ndarray &nOmega, const double &DTime) {
-    mat D = array2mat(nD);
-    mat Omega = array2mat(nOmega);
-    return mat2array(simcoon::Delta_log_strain(D, Omega, DTime));
+py::array_t<double> Delta_log_strain(const py::array_t<double> &D, const py::array_t<double> &Omega, const double &DTime, const bool &copy) {
+    mat D_cpp = carma::arr_to_mat(D);
+    mat Omega_cpp = carma::arr_to_mat(Omega);
+    mat Delta_log_strain = simcoon::Delta_log_strain(D_cpp, Omega_cpp, DTime)
+    return carma::mat_to_arr(Delta_log_strain, copy);
 }
     
 } //namepsace simpy
