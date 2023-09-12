9f9499f2a519244b3e959cd315dd1a942ade39f7
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-12 17:52:32 +0200

    Add option to build carma on conda

--- carma.recipe/bld.bat
+++ carma.recipe/bld.bat
@@ -1,6 +1,7 @@
 cmake -S . -B build ^
       -G "Visual Studio 17 2022" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%
+      -DCMAKE_INSTALL_PREFIX:PATH=%CONDA_PREFIX%
+      -DCARMA_INSTALL_LIB=ON
 cmake --build build --target ALL_BUILD --config Release
 cmake --install build
