5bb1424f9a4efb52f16e33184804de8aa6b4674d
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-18 22:58:27 +0200

    test with PREFIX

--- carma.recipe/build.sh
+++ carma.recipe/build.sh
@@ -4,6 +4,6 @@ set -x
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:path=$CONDA_PREFIX -DCARMA_INSTALL_LIB=ON
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:path=$PREFIX -DCARMA_INSTALL_LIB=ON
 make
 make install
