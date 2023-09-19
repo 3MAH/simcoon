58aa9b67e83150c397bcc8868c3bfc66e5ec6765
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-19 15:02:52 +0200

    Update bld.bat

--- carma.recipe/bld.bat
+++ carma.recipe/bld.bat
@@ -2,6 +2,6 @@ cmake -S . -B build ^
       -G "Visual Studio 17 2022" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%
-      -DPython3_ROOT_DIR=%PREFIX%
+      -DPython3_EXECUTABLE=$(which python3)
 cmake --build build --target ALL_BUILD --config Release
 cmake --install build
