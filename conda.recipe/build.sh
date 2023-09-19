65074572775228197009ff1d5ff2619acf89fdda
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-19 23:53:29 +0200

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -6,9 +6,9 @@ cd $SRC_DIR
 
 cmake ${CMAKE_ARGS} -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_INSTALL_PREFIX:path=$PREFIX
-  -D CMAKE_INCLUDE_PATH=$PREFIX/include
-  -D CMAKE_LIBRARY_PATH=$PREFIX/lib
+  -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
+  -D CMAKE_INCLUDE_PATH=$PREFIX/include \
+  -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
 
 cmake --build build --config Release
 cmake --install build
@@ -17,14 +17,12 @@ cd $SRC_DIR/simcoon-python-builder
 
 cmake ${CMAKE_ARGS} -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_INSTALL_PREFIX:path=$PREFIX
-  -D CMAKE_INCLUDE_PATH=$PREFIX/include
+  -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
+  -D CMAKE_INCLUDE_PATH=$PREFIX/include \
   -D CMAKE_LIBRARY_PATH=$PREFIX/lib
 
 cmake --build build --config Release
 cmake --install build
-
-mkdir -p build
 cp lib/simmit.so $SRC_DIR/python-setup/simcoon/
 
 cd $SRC_DIR/python-setup
