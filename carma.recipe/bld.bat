f703f9d9fa76d747974d1044621c72652fc4107e
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-19 22:59:14 +0200

    use Ninja

--- carma.recipe/bld.bat
+++ carma.recipe/bld.bat
@@ -1,6 +1,8 @@
-cmake -S . -B build ^
-      -G "Visual Studio 17 2022" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%
+cmake -G Ninja ^
+      -D CMAKE_BUILD_TYPE:STRING=Release ^
+      -D CMAKE_INSTALL_PREFIX:PATH=%PREFIX%
+      -D CARMA_INSTALL_LIB=ON
+      -S . -B build ^      
+
 cmake --build build --target ALL_BUILD --config Release
 cmake --install build
