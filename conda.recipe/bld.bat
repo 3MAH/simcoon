be684ba0f1a8b5307e1b2692525f56147f214c33
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-19 23:42:33 +0200

    Update bld.bat while suppressing arma2numy

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,44 +1,23 @@
-:: Armadillo
-git clone https://gitlab.com/conradsnicta/armadillo-code.git armadillo
-cd armadillo
-copy /y %SRC_DIR%\armadillo\examples\lib_win64\libopenblas.dll %PREFIX%\Library\bin\
-copy /y %SRC_DIR%\armadillo\examples\lib_win64\libopenblas.lib %PREFIX%\Library\lib\
-cmake -S . -B build -G "Visual Studio 17 2022" -DBLAS_LIBRARY:FILEPATH="%PREFIX%\Library\lib\libopenblas.lib" -DLAPACK_LIBRARY:FILEPATH="%PREFIX%\Library\lib\libopenblas.lib"
-cmake --build build --config RELEASE
-cmake --install build --prefix %PREFIX%\Library
-if errorlevel 1 exit 1
-
 :: Simcoon
 cd %SRC_DIR%
-cmake -S . -B build ^
-      -G"Visual Studio 17 2022" ^
+cmake -G Ninja ^
       -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -Wno-dev
-cmake --build build --target simcoon --config Release
-cmake --install build
-if errorlevel 1 exit 1
+      -Wno-dev ^
+      -S . -B build
 
-:: Arma2numpy
-cd arma2numpy-builder
-cmake -S . -B build ^
-      -G "Visual Studio 17 2022" ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DPython3_ROOT_DIR=%PREFIX% ^
-      -Wno-dev 
-cmake --build build --target ALL_BUILD --config Release
+cmake --build build --target simcoon --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
 :: Python binding
 cd %SRC_DIR%\simcoon-python-builder
-cmake -S . -B build ^
-      -G "Visual Studio 17 2022" ^
+cmake -G Ninja ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DPython3_ROOT_DIR=%PREFIX% ^
-      -Wno-dev
+      -Wno-dev ^
+      -S . -B build
 cat build\CMakeFiles\CMakeOutput.log
 dir build\CMakeFiles
 cat build\CMakeFiles\CMakeError.log
@@ -54,7 +33,5 @@ type NUL > __init__.py
 xcopy /s /i %SRC_DIR%\simcoon-python-builder\build\lib\Release\simmit.pyd %SP_DIR%\simcoon
 if errorlevel 1 exit 1
 
-REM xcopy /s /i %SRC_DIR%\armadillo\examples\lib_win64\libopenblas.dll %PREFIX%\Library\bin
 xcopy /s /i %SRC_DIR%\build\lib\Release\simcoon.dll %PREFIX%\Library\bin
-xcopy /s /i %SRC_DIR%\arma2numpy-builder\build\bin\Release\arma2numpy.dll %PREFIX%\Library\bin
 if errorlevel 1 exit 1
