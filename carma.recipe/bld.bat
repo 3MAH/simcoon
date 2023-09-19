c6b5f1a9e104cfefb336215f372d28794302ab37
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-19 23:03:45 +0200

    fix typos

--- carma.recipe/bld.bat
+++ carma.recipe/bld.bat
@@ -1,8 +1,8 @@
 cmake -G Ninja ^
       -D CMAKE_BUILD_TYPE:STRING=Release ^
-      -D CMAKE_INSTALL_PREFIX:PATH=%PREFIX%
-      -D CARMA_INSTALL_LIB=ON
-      -S . -B build ^      
+      -D CMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
+      -D CARMA_INSTALL_LIB=ON ^
+      -S . -B build
 
 cmake --build build --target ALL_BUILD --config Release
 cmake --install build
--- carma.recipe/build.sh
+++ carma.recipe/build.sh
@@ -6,8 +6,8 @@ cd $SRC_DIR
 
 cmake ${CMAKE_ARGS} -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_INSTALL_PREFIX:path=$PREFIX
-  -D DCARMA_INSTALL_LIB=ON \
+  -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
+  -D DCARMA_INSTALL_LIB=ON
 
 cmake --build build --target ALL_BUILD --config Release
 cmake --install build
