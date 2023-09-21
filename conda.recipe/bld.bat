ce11affea975e2dd9fd59b71452e920372c0c4ea
Yves Chemisky <yves.chemisky@gmail.com>
2023-09-21 09:29:25 +0200

    Update bld.bat

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -10,6 +10,19 @@ cmake --build build --target simcoon --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
+:: Arma2numpy binding
+cd %SRC_DIR%\arma2numpy-builder
+cmake -G Ninja ^
+      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DPython3_ROOT_DIR=%PREFIX% ^
+      -Wno-dev ^
+      -S . -B build
+      
+cmake --build build --target arma2numpy --config Release
+cmake --install build
+if errorlevel 1 exit 1
+
 :: Python binding
 cd %SRC_DIR%\simcoon-python-builder
 cmake -G Ninja ^
@@ -18,20 +31,17 @@ cmake -G Ninja ^
       -DPython3_ROOT_DIR=%PREFIX% ^
       -Wno-dev ^
       -S . -B build
-cat build\CMakeFiles\CMakeOutput.log
-dir build\CMakeFiles
-cat build\CMakeFiles\CMakeError.log
-cmake --build build --target ALL_BUILD --config Release
+
+cmake --build build --target simmit --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
 :: Install simcoon python 
-cd %SP_DIR%
-mkdir simcoon
-cd simcoon
-type NUL > __init__.py
-xcopy /s /i %SRC_DIR%\simcoon-python-builder\build\lib\Release\simmit.pyd %SP_DIR%\simcoon
+%SRC_DIR%\python-setup
+xcopy /s /i %SRC_DIR%\simcoon-python-builder\build\lib\Release\simmit.pyd %SRC_DIR%\python-setup\simcoon
 if errorlevel 1 exit 1
 
-xcopy /s /i %SRC_DIR%\build\lib\Release\simcoon.dll %PREFIX%\Library\bin
-if errorlevel 1 exit 1
+%PYTHON% -m pip install .
+
+:: xcopy /s /i %SRC_DIR%\build\lib\Release\simcoon.dll %PREFIX%\Library\bin
+:: if errorlevel 1 exit 1
