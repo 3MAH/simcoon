c352a8a74d66231d48b8eb84b2c85a43dc6d385e
pruliere <etienne.pruliere@ensam.eu>
2023-09-26 14:27:54 +0200

    update bld.bat to correct a bug (typo ?)

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,11 +1,11 @@
 :: Simcoon
 cd %SRC_DIR%
 cmake -G"Visual Studio 17 2022" ^
+      -S . -B build ^
       -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
-      -Wno-dev ^
-      -S . -B build
+      -Wno-dev
 
 cmake --build build --target simcoon --config Release
 cmake --install build
@@ -13,27 +13,27 @@ if errorlevel 1 exit 1
 
 :: Arma2numpy binding
 cd %SRC_DIR%\arma2numpy-builder
-cmake -G"Visual Studio 17 2022" ^
+cmake -G "Visual Studio 17 2022" ^
+      -S . -B build ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DPython3_ROOT_DIR=%PREFIX% ^
-      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^      
-      -Wno-dev ^
-      -S . -B build
-      
+      -DPython3_EXECUTABLE=%PREFIX% ^
+      -Wno-dev
+
 cmake --build build --target arma2numpy --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
 :: Python binding
 cd %SRC_DIR%\simcoon-python-builder
-cmake -G"Visual Studio 17 2022" ^
+cmake -G "Visual Studio 17 2022" ^
+      -S . -B build ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DPython3_ROOT_DIR=%PREFIX% ^
-      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
-      -Wno-dev ^
-      -S . -B build
+      -DPython3_EXECUTABLE=%PREFIX% ^
+      -Wno-dev
 
 cmake --build build --target simmit --config Release
 cmake --install build
