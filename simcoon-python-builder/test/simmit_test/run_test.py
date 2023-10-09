49c2b16b896b629255c90191daa2afe785d856ef
Yves Chemisky <yves.chemisky@gmail.com>
2023-10-09 11:32:36 +0200

    Update run_test.py

--- simcoon-python-builder/test/simmit_test/run_test.py
+++ simcoon-python-builder/test/simmit_test/run_test.py
@@ -1,7 +1,21 @@
-#!/usr/bin/python
-
+import pytest
 import numpy as np
-#import Tarma2numpy
+import numpy.typing as npt
 from simcoon import simmit as sim
 
-L = sim.L_iso([1., 0.3], 'Enu')
+@pytest.fixture(scope='session')
+def L_gt() -> npt.NDArray[np.float_]:
+    L_gt = np.array([
+        [1.34615385, 0.57692308, 0.57692308, 0., 0., 0.],
+        [0.57692308, 1.34615385, 0.57692308, 0., 0., 0.],
+        [0.57692308, 0.57692308, 1.34615385, 0., 0., 0.],
+        [0., 0., 0., 0.38461538, 0., 0.],
+        [0., 0., 0., 0., 0.38461538, 0.],
+        [0., 0., 0., 0., 0., 0.38461538]
+    ])
+    return L_gt
+
+def test_L_iso(L_gt):
+    """Test L_iso with E=1., nu=0.3."""
+    L = sim.L_iso([1., 0.3], 'Enu')
+    assert np.allclose(L, L_gt)
