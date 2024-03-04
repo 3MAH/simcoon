f2dc0e42db0730f798ec586eeefb26a9504970c9
Yves Chemisky <yves.chemisky@gmail.com>
2024-03-04 22:18:04 +0100

    replaced boost::python with pybind11

--- simcoon-python-builder/src/python_wrappers/Libraries/Identification/constants.cpp
+++ simcoon-python-builder/src/python_wrappers/Libraries/Identification/constants.cpp
@@ -1,64 +1,57 @@
-///@file constants.cpp
-///@brief Handle of input constants exposed in python
-///@version 0.9
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
-
-#include <iostream>
-#include <fstream>
-#include <assert.h>
-#include <math.h>
+#include <string>
+#include <carma>
 #include <armadillo>
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+#include <assert.h>
 
-#include <simcoon/arma2numpy/list_vector.hpp>
-#include <simcoon/arma2numpy/numpy_arma.hpp>
 #include <simcoon/Simulation/Identification/constants.hpp>
 #include <simcoon/python_wrappers/Libraries/Identification/constants.hpp>
 
 using namespace std;
 using namespace arma;
-using namespace arma2numpy;
-namespace bp = boost::python;
-namespace bn = boost::python::numpy;
+namespace py=pybind11;
 
 namespace simpy{
     
 //-------------------------------------------------------------
-simcoon::constants build_constants_full(const int &mnumber, const double &mvalue, const bn::ndarray &minput_values, const std::string &mkey, const int &mninput_files, const bp::list &minput_files)
+simcoon::constants build_constants_full(const int &mnumber, const double &mvalue, const py::array_t<double> &minput_values, const std::string &mkey, const int &mninput_files, const py::list &minput_files)
 //-------------------------------------------------------------
 {
     simcoon::constants a;
     a.number = mnumber;
     a.value = mvalue;
-    a.input_values = array2vec(minput_values);
+    a.input_values = carma::arr_to_col(minput_values);
     a.key = mkey;
     a.ninput_files = mninput_files;
-    a.input_files = py_list_to_std_vector_string(minput_files);
+    a.input_files = minput_files.cast<std::vector<std::string>>();
     return a;
 }
 
 //------------------------------------------------------
-bn::ndarray constants_get_input_values(simcoon::constants &self) {
-    return vec2array(self.input_values);
+py::array_t<double> constants_get_input_values(simcoon::constants &self) {
+    return carma::col_to_arr(self.input_values);
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-bp::list constants_get_input_files(simcoon::constants &self) {
-    return std_vector_to_py_list_string(self.input_files);
+py::list constants_get_input_files(simcoon::constants &self) {
+    py::list list_to_return = py::cast(self.input_files);
+    return list_to_return;
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-void constants_set_input_values(simcoon::constants &self, const bn::ndarray &minput_values) {
-    self.input_values = array2vec(minput_values);
+void constants_set_input_values(simcoon::constants &self, const py::array_t<double> &minput_values) {
+    self.input_values = carma::arr_to_col(minput_values);
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-void constants_set_input_files(simcoon::constants &self, const bp::list &minput_values) {
-    self.input_files = py_list_to_std_vector_string(minput_values);
+void constants_set_input_files(simcoon::constants &self, const py::list &minput_files) {
+    self.input_files = minput_files.cast<std::vector<std::string>>();
     self.ninput_files = self.input_files.size();
 }
 //------------------------------------------------------
