c6fd71e04a2dfb915f8fbc044352cad06393d3c7
Yves Chemisky <yves.chemisky@gmail.com>
2024-03-29 15:56:11 +0100

    suppress unit_cell module

--- simcoon-python-builder/include/simcoon/python_wrappers/Libraries/Unit_cell/write.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <boost/python.hpp>
-#include <simcoon/Continuum_mechanics/Unit_cell/write.hpp>
-
-namespace simpy{
-
-void simcoon::write_run_perturbation_file(const boost::python::str &, const boost::python::str &);
-    
-} //namespace simpy
