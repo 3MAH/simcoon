1e05a0a839cd79ccd844c2d1d6cd0e899906e157
Yves Chemisky <yves.chemisky@gmail.com>
2024-04-09 13:07:17 +0200

    fix conda build

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -11,20 +11,6 @@ cmake --build build --target simcoon --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
-:: Arma2numpy binding
-cd %SRC_DIR%\arma2numpy-builder
-cmake -G "Visual Studio 17 2022" ^
-      -S . -B build ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DPython3_ROOT_DIR=%PREFIX% ^
-      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
-      -Wno-dev
-
-cmake --build build --target arma2numpy --config Release
-cmake --install build
-if errorlevel 1 exit 1
-
 :: Python binding
 cd %SRC_DIR%\simcoon-python-builder
 cmake -G "Visual Studio 17 2022" ^
@@ -47,7 +33,6 @@ xcopy /s /i %SRC_DIR%\simcoon-python-builder\build\lib\Release\simmit.pyd %SP_DI
 if errorlevel 1 exit 1
 
 xcopy /s /i %SRC_DIR%\build\lib\Release\simcoon.dll %PREFIX%\Library\bin
-xcopy /s /i %SRC_DIR%\arma2numpy-builder\build\bin\Release\arma2numpy.dll %PREFIX%\Library\bin
 if errorlevel 1 exit 1
 
 
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -13,17 +13,6 @@ cmake ${CMAKE_ARGS} -S . -B build \
 cmake --build build -j${CPU_COUNT} --target simcoon --config Release
 cmake --install build
 
-cd $SRC_DIR/arma2numpy-builder
-
-cmake ${CMAKE_ARGS} -S . -B build \
-  -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
-  -D CMAKE_INCLUDE_PATH=$PREFIX/include \
-  -D CMAKE_LIBRARY_PATH=$PREFIX/lib
-  
-cmake --build build -j${CPU_COUNT} --target arma2numpy --config Release
-cmake --install build
-
 cd $SRC_DIR/simcoon-python-builder
 
 cmake ${CMAKE_ARGS} -S . -B build \
