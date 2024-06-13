b68baf679346d711fa67458afaed5a14994c6934
Yves Chemisky <yves.chemisky@gmail.com>
2024-06-13 17:42:44 +0200

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,25 +4,25 @@ set -ex
 
 cd $SRC_DIR
 
-cmake ${CMAKE_ARGS} -S . -B build \
+cmake ${CMAKE_ARGS} -G Ninja -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
   -D CMAKE_INCLUDE_PATH=$PREFIX/include \
   -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
 
-cmake --build build -j${CPU_COUNT} --target simcoon --config Release
-cmake --install build
+cmake -G Ninja --build build -j${CPU_COUNT} --target simcoon --config Release
+cmake -G Ninja --install build
 
 cd $SRC_DIR/simcoon-python-builder
 
-cmake ${CMAKE_ARGS} -S . -B build \
+cmake ${CMAKE_ARGS} -G Ninja -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX:path=$PREFIX \
   -D CMAKE_INCLUDE_PATH=$PREFIX/include \
   -D CMAKE_LIBRARY_PATH=$PREFIX/lib
 
-cmake --build build -j${CPU_COUNT} --target simmit --config Release
-cmake --install build
+cmake -G Ninja --build build -j${CPU_COUNT} --target simmit --config Release
+cmake -G Ninja --install build
 cp $SRC_DIR/simcoon-python-builder/build/lib/simmit.so $SRC_DIR/python-setup/simcoon/
 
 cd $SRC_DIR/python-setup
