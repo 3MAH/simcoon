21a79e9cbf2212fca03b86ac893385849e26b136
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-09-26 14:14:19 +0200

    Initial commit

---
+++ python-setup/simcoon/data.py
@@ -0,0 +1,12 @@
+"""
+Data Class to manage Simcoon computation data
+"""
+
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class Data:
+    """
+    Data class to manage input or output Simcoon computation data
+    """
