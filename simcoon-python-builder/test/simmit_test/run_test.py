f5d7c8e2d3d6867a19c28548a3cfda90e7a03d56
Yves Chemisky <yves.chemisky@gmail.com>
2024-09-27 12:00:07 +0200

    Update run_test.py

--- simcoon-python-builder/test/simmit_test/run_test.py
+++ simcoon-python-builder/test/simmit_test/run_test.py
@@ -3,19 +3,34 @@ import numpy as np
 import numpy.typing as npt
 from simcoon import simmit as sim
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def L_gt() -> npt.NDArray[np.float_]:
-    L_gt = np.array([
-        [1.34615385, 0.57692308, 0.57692308, 0., 0., 0.],
-        [0.57692308, 1.34615385, 0.57692308, 0., 0., 0.],
-        [0.57692308, 0.57692308, 1.34615385, 0., 0., 0.],
-        [0., 0., 0., 0.38461538, 0., 0.],
-        [0., 0., 0., 0., 0.38461538, 0.],
-        [0., 0., 0., 0., 0., 0.38461538]
-    ])
+    L_gt = np.array(
+        [
+            [1.34615385, 0.57692308, 0.57692308, 0.0, 0.0, 0.0],
+            [0.57692308, 1.34615385, 0.57692308, 0.0, 0.0, 0.0],
+            [0.57692308, 0.57692308, 1.34615385, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.38461538, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.38461538, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.38461538],
+        ]
+    )
     return L_gt
+
 
 def test_L_iso(L_gt):
     """Test L_iso with E=1., nu=0.3."""
-    L = sim.L_iso([1., 0.3], 'Enu')
+    L = sim.L_iso([1.0, 0.3], "Enu")
     assert np.allclose(L, L_gt)
+
+
+def test_isochoric_invariants():
+
+    V = np.random.rand(3, 3) + np.eye(3, 3)
+    V = 0.5 * (V + V.transpose())
+    b = V * V
+    lambda_bar = sim.isochoric_invariants(b)
+    b_vec = sim.t2v_strain(b)
+    lambda_bar_vec = sim.isochoric_invariants(b_vec)
+    assert np.allclose(lambda_bar, lambda_bar_vec)
