02909f0c5f1eae2789156213d4e9265af20b45cb
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-09-27 16:06:22 +0200

    First implementation for Data class

--- python-setup/simcoon/data.py
+++ python-setup/simcoon/data.py
@@ -2,11 +2,30 @@
 Data Class to manage Simcoon computation data
 """
 
-from dataclasses import dataclass
-import numpy as np
 
-@dataclass
+from typing import List, Union
+import numpy as np
+import numpy.typing as npt
+
 class Data:
     """
     Data class to manage input or output Simcoon computation data
     """
+
+    def __init__(self, list_data: Union[List[npt.NDArray[np.float_]], List[str]]):
+
+        if type(list_data) == List[str]:
+            tmp_list = []
+            for i in range(len(list_data)):
+                tmp_list[i] = np.loadtxt(list_data[i])
+            self.list_data = tmp_list
+        elif type(list_data) == List[npt.NDArray[np.float_]]:
+            self.list_data = list_data
+        else:
+            raise TypeError("list_data must be a list of numpy arrays or of strings")
+
+    def write_input_file(self):
+        pass
+
+    def write_tab_file(self):
+        pass
