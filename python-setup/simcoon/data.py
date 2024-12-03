39e10d59ec8187659006d2d4261ae9e6a7b80206
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-12-03 16:24:03 +0100

    Implement write_files_exp

--- python-setup/simcoon/data.py
+++ python-setup/simcoon/data.py
@@ -2,10 +2,10 @@
 Data Class to manage computation data
 """
 
-
 from typing import List, Union, Optional
 import numpy as np
 import numpy.typing as npt
+import glob
 
 
 class Data:
@@ -21,29 +21,53 @@ class Data:
         self.control = control
         self.observation = observation
         self.timestep = timestep
-        self.increment = np.array([i+1 for i in range(np.shape(self.control)[0])])
+        self.increments: npt.NDArray[np.float_] = np.asarray([i + 1 for i in range(np.shape(self.control)[0])])
         if time is None:
-            self.time = np.array([i*timestep for i in range(np.shape(self.control)[0])])
+            self.time = np.asarray([i * timestep for i in range(np.shape(self.control)[0])])
         else:
             self.time = time
 
 
-def write_input_and_tab_files(self, exp_data_path: str = "exp_data/", tab_files_path: str = "data/") -> None:
-    for element in self.list_data:  # order: strain, stress
+def write_input_and_tab_files(list_data: List[Data], exp_data_path: str = "exp_data/",
+                              tab_files_path: str = "data/") -> None:
+    for element in list_data:
         i = 1
-        increments = np.array([j + 1 for j in range(len(element))])
-        time = np.array([j * 0.01 for j in range(len(element))])
-        strain = element[:, 0]
-        exp_data_array = np.column_stack((increments, time, element))
-        tab_file_array = np.column_stack((increments, time, strain))
-        np.savetxt(exp_data_path + "input_data_" + str(i) + ".txt", exp_data_array)
-        np.savetxt(tab_files_path + "tab_file_" + str(i) + ".txt", tab_file_array)
+        exp_data_array = np.column_stack((element.increments, element.time, element.control, element.observation))
+        tab_file_array = np.column_stack((element.increments, element.time, element.control))
+        np.savetxt(exp_data_path + "input_data_" + f'{i: 02}' + ".txt", exp_data_array)
+        np.savetxt(tab_files_path + "tab_file_" + f'{i: 02}' + ".txt", tab_file_array)
         i += 1
 
 
-def write_files_exp(self, list_nb_columns_in_files: List[int], list_nb_columns_to_identify: List[int], list_columns_to_identify: List[int], path: str = "data/", ) -> None:
-    pass
+def write_files_exp(list_data: List[Data], path: str = "data/", exp_data_path: str = "exp_data/") -> None:
+    list_exp_input_files_names = glob.glob("input_data_*.txt", root_dir=exp_data_path)
+    list_nb_columns_in_files = []
+    list_nb_observation_columns = []
+    list_observation_columns_indices = []
+    for element in list_data:
+        nb_columns_in_files = element.increments.ndim + element.time.ndim + element.control.ndim + \
+                              element.observation.ndim
+        nb_observation_columns = element.observation.ndim
+        observation_columns_indices = [i for i in range(nb_columns_in_files - element.observation.ndim, nb_columns_in_files)]
+        list_nb_columns_in_files.append(nb_columns_in_files)
+        list_nb_observation_columns.append(nb_observation_columns)
+        list_observation_columns_indices.append(observation_columns_indices)
+    with open(path + "files_exp.inp", "w+") as file:
+        file.write("#Name_of_the_exp_files\n")
+        for file_name in list_exp_input_files_names:
+            file.write(file_name + "\n")
+        file.write("\n#EXP_Nb_columns_in_files\n")
+        for nb_col_file in list_nb_columns_in_files:
+            file.write(str(nb_col_file) + "\n")
+        file.write("\n#EXP_Nb_columns_to_identify\n")
+        for nb_obs_col in list_nb_observation_columns:
+            file.write(str(nb_obs_col) + "\n")
+        file.write("\n#EXP_colums_to_identify\n")
+        for indices_list in list_observation_columns_indices:
+            file.write(' '.join(str(val) for val in indices_list))
+            file.write("\n")
 
 
-def write_files_num(self, list_nb_columns_in_files: List[int], list_columns_to_identify: List[int], path: str = "data/", ) -> None:
+def write_files_num(list_nb_columns_in_files: List[int], list_columns_to_identify: List[int],
+                    path: str = "data/", ) -> None:
     pass
