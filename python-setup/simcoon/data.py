61e6b21c90edb6292ae0b7c2c2b948b1e21b5052
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-12-04 20:21:49 +0100

    Fix string padding issue

--- python-setup/simcoon/data.py
+++ python-setup/simcoon/data.py
@@ -34,8 +34,8 @@ def write_input_and_tab_files(list_data: List[Data], exp_data_path: str = "exp_d
     for element in list_data:
         exp_data_array = np.column_stack((element.increments, element.time, element.control, element.observation))
         tab_file_array = np.column_stack((element.increments, element.time, element.control))
-        np.savetxt(exp_data_path + "input_data_"+f'{i: 02}'+".txt", exp_data_array)
-        np.savetxt(tab_files_path + "tab_file_"+f'{i: 02}'+".txt", tab_file_array)
+        np.savetxt(exp_data_path + f"input_data_{i:02}.txt", exp_data_array)
+        np.savetxt(tab_files_path + "tab_file_"+f'{i:02}'+".txt", tab_file_array)
         i += 1
 
 
