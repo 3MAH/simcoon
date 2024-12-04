dc141f046cf73515c1a20960ea55013c6b357744
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-12-04 19:55:14 +0100

    Fix bad iteration start

--- python-setup/simcoon/data.py
+++ python-setup/simcoon/data.py
@@ -30,12 +30,12 @@ class Data:
 
 def write_input_and_tab_files(list_data: List[Data], exp_data_path: str = "exp_data/",
                               tab_files_path: str = "data/") -> None:
+    i = 1
     for element in list_data:
-        i = 1
         exp_data_array = np.column_stack((element.increments, element.time, element.control, element.observation))
         tab_file_array = np.column_stack((element.increments, element.time, element.control))
-        np.savetxt(exp_data_path + "input_data_" + f'{i: 02}' + ".txt", exp_data_array)
-        np.savetxt(tab_files_path + "tab_file_" + f'{i: 02}' + ".txt", tab_file_array)
+        np.savetxt(exp_data_path + "input_data_"+f'{i: 02}'+".txt", exp_data_array)
+        np.savetxt(tab_files_path + "tab_file_"+f'{i: 02}'+".txt", tab_file_array)
         i += 1
 
 
