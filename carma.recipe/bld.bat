a98e1f23a2846cdb3baa04857ac0b924e3be3f16
pruliere <62881513+pruliere@users.noreply.github.com>
2025-01-23 11:51:41 +0100

    undo last commit

--- carma.recipe/bld.bat
+++ carma.recipe/bld.bat
@@ -1,6 +1,8 @@
-cmake -S . -B build ^
+cmake -G"Visual Studio 17 2022" ^
       -D CMAKE_BUILD_TYPE:STRING=Release ^
-      -D CMAKE_INSTALL_PREFIX=%PREFIX%/Library
+      -D CMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+      -D CARMA_INSTALL_LIB=ON ^
+      -S . -B build
 
 cmake --build build --config Release
 cmake --install build
