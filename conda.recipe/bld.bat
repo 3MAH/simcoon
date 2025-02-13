c42dc2c187d422bb96798a0916906bbd1136ccbe
pruliere <62881513+pruliere@users.noreply.github.com>
2025-02-13 15:45:23 +0100

    Update bld.bat

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,31 +1,24 @@
 :: Simcoon
 cd %SRC_DIR%
-cmake -G"Visual Studio 17 2022" ^
-      -S . -B build ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+cmake -S . -B build ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
+      -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^      
       -Wno-dev
 
-cmake --build build --target simcoon --config Release
+cmake --build build --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
 :: Python binding
-cd %SRC_DIR%\simcoon-python-builder
-cmake -G "Visual Studio 17 2022" ^
-      -S . -B build ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+cmake -S simcoon-python-builder -B simcoon-python-builder/build ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DPython3_ROOT_DIR=%PREFIX% ^
-      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
+      -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
       -Wno-dev
 
-cmake --build build --target simmit --config Release
-cmake --install build
+cmake --build simcoon-python-builder/build --config Release
+cmake --install simcoon-python-builder/build
 if errorlevel 1 exit 1
 
-xcopy /s /i /y %SRC_DIR%\simcoon-python-builder\build\lib\simmit.pyd %SRC_DIR%\python-setup\simcoon\
+copy %SRC_DIR%/simcoon-python-builder/build/lib/simmit.pyd %SRC_DIR%/python-setup/simcoon/simmit.pyd
 python -m pip install %SRC_DIR%/python-setup
-
 if errorlevel 1 exit 1
