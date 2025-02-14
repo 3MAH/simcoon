d4f9047de4d2484ba9358edd8e38c2405569601e
pruliere <62881513+pruliere@users.noreply.github.com>
2025-02-14 08:54:18 +0100

    Update bld.bat

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -2,22 +2,25 @@
 cd %SRC_DIR%
 cmake -G"Visual Studio 17 2022" ^
       -S . -B build ^
-      -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
       -Wno-dev
 
-cmake --build build --config Release
+cmake --build build --target simcoon --config Release
 cmake --install build
 if errorlevel 1 exit 1
 
 :: Python binding
 cmake -G"Visual Studio 17 2022" ^
       -S simcoon-python-builder -B simcoon-python-builder/build ^
-      -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DPython3_ROOT_DIR=%PREFIX% ^
+      -DPython3_EXECUTABLE=%PREFIX%/python.exe ^
       -Wno-dev
 
-cmake --build simcoon-python-builder/build --config Release
+cmake --build simcoon-python-builder/build --target simmit --config Release
 cmake --install simcoon-python-builder/build
 if errorlevel 1 exit 1
 
