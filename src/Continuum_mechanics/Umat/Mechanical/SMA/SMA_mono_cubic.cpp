3bf41d039ab73cf0c197404975b8eaec3124101d
Yves Chemisky <yves.chemisky@gmail.com>
2025-04-14 16:43:04 +0200

    Update SMA_mono_cubic.cpp

--- src/Continuum_mechanics/Umat/Mechanical/SMA/SMA_mono_cubic.cpp
+++ src/Continuum_mechanics/Umat/Mechanical/SMA/SMA_mono_cubic.cpp
@@ -400,11 +400,9 @@ void umat_sma_mono_cubic(const vec &Etot, const vec &DEtot, vec &sigma, mat &Lt,
         }
         else
             Lt = L;
-       
-		vec eigval = eig_sym(Lt);  								     		
-		
 		///Note : To use with Self-consistent micromechanics only!
-/*		while(eigval(0) < 0.) {
+/*		vec eigval = eig_sym(Lt);  								     				
+		while(eigval(0) < 0.) {
 				Lt = 0.99*Lt + 0.01*Lt_eff;
 				eigval = eig_sym(Lt);			
 		}       */
