cf979f03fc02bad77dfafb248835335d6fa95f27
Kevin Marchais <kevinmarchais@gmail.com>
2025-07-20 16:33:45 +0200

    since the return type of boost::dll::import_symbol depends on boost's version, use auto

--- src/Continuum_mechanics/Umat/umat_smart.cpp
+++ src/Continuum_mechanics/Umat/umat_smart.cpp
@@ -779,9 +779,7 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
 
             fs::path lib_path("external");  // Path to the directory with our plugin library
             fs::path boost_lib_path = lib_path / "umat_plugin_ext";            
-            std::shared_ptr<umat_plugin_ext_api> external_umat;
-
-            external_umat = boost::dll::import_symbol<umat_plugin_ext_api>(  // Type of imported symbol is between `<` and `>`
+            auto external_umat = boost::dll::import_symbol<umat_plugin_ext_api>(  // Type of imported symbol is between `<` and `>`
                 boost_lib_path,  // Path to the library and library name
                 "external_umat",               // Name of the symbol to import
                 boost::dll::load_mode::append_decorations  // Handles platform-specific library name decorations
@@ -795,9 +793,7 @@ void select_umat_M(phase_characteristics &rve, const mat &DR,const double &Time,
             //
             fs::path lib_path("external");  // Path to the directory with our plugin library
             fs::path boost_lib_path = lib_path / "umat_plugin_aba";            
-            std::shared_ptr<umat_plugin_aba_api> abaqus_umat;
-
-            abaqus_umat = boost::dll::import_symbol<umat_plugin_aba_api>(boost_lib_path,
+            auto abaqus_umat = boost::dll::import_symbol<umat_plugin_aba_api>(boost_lib_path,
                 "abaqus_umat", 
                 boost::dll::load_mode::append_decorations
             );
