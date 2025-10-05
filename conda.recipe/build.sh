112450c2e77872909a1454dee5b2247cc899a79b
Kevin Marchais <kevinmarchais@gmail.com>
2025-10-05 23:40:30 +0200

    Fix CMake variable syntax in Linux build script
    - Remove space after -D flag (use -DVAR instead of -D VAR)
    - Remove CMAKE_PREFIX_PATH override that was breaking conda's paths
    - Let pybind11_DIR alone handle pybind11 discovery

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -9,15 +9,14 @@ PYBIND11_DIR=$($PYTHON -c "import pybind11; print(pybind11.get_cmake_dir())")
 
 # Configure with Python bindings enabled
 cmake ${CMAKE_ARGS} -G Ninja -S . -B build \
-  -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-  -D CMAKE_INCLUDE_PATH=$PREFIX/include \
-  -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
-  -D SIMCOON_BUILD_PYTHON_BINDINGS:BOOL=ON \
-  -D SIMCOON_BUILD_TESTS:BOOL=ON \
-  -D Python3_EXECUTABLE:FILEPATH=$PYTHON \
-  -D pybind11_DIR:PATH=$PYBIND11_DIR \
-  -D CMAKE_PREFIX_PATH=$PYBIND11_DIR
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+  -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
+  -DSIMCOON_BUILD_PYTHON_BINDINGS:BOOL=ON \
+  -DSIMCOON_BUILD_TESTS:BOOL=ON \
+  -DPython3_EXECUTABLE:FILEPATH=$PYTHON \
+  -Dpybind11_DIR:PATH=$PYBIND11_DIR
 
 # Build everything (C++ library and Python bindings)
 cmake --build build -j${CPU_COUNT}
