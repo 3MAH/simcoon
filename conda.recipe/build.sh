c6eddbdb913201e014e38071c82f095f2bc3ce0c
Kevin Marchais <kevinmarchais@gmail.com>
2025-10-05 23:34:34 +0200

    Add Python bindings configuration to Linux/macOS build script
    Add explicit CMake flags to enable Python bindings:
    - SIMCOON_BUILD_PYTHON_BINDINGS:BOOL=ON
    - Python3_EXECUTABLE and pybind11_DIR paths
    - Match configuration used in Windows bld.bat

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,12 +4,20 @@ set -ex
 
 cd $SRC_DIR
 
+# Set pybind11 path for CMake
+PYBIND11_DIR=$($PYTHON -c "import pybind11; print(pybind11.get_cmake_dir())")
+
 # Configure with Python bindings enabled
 cmake ${CMAKE_ARGS} -G Ninja -S . -B build \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
   -D CMAKE_INCLUDE_PATH=$PREFIX/include \
-  -D CMAKE_LIBRARY_PATH=$PREFIX/lib
+  -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
+  -D SIMCOON_BUILD_PYTHON_BINDINGS:BOOL=ON \
+  -D SIMCOON_BUILD_TESTS:BOOL=ON \
+  -D Python3_EXECUTABLE:FILEPATH=$PYTHON \
+  -D pybind11_DIR:PATH=$PYBIND11_DIR \
+  -D CMAKE_PREFIX_PATH=$PYBIND11_DIR
 
 # Build everything (C++ library and Python bindings)
 cmake --build build -j${CPU_COUNT}
