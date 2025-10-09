4f824ac361757c68a88372998b3badc459e2d773
Yves Chemisky <yves.chemisky@gmail.com>
2025-10-09 14:09:07 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,21 +13,33 @@
 import mock
 import os
 import sys
+import importlib.util
+from sphinx_gallery.sorting import FileNameSortKey
 
 sys.path.insert(
     0, os.path.abspath("../../simcoon-python-builder/include/simcoon/python_wrappers")
 )
 
-
-# -- Project information -----------------------------------------------------
-
 project = "Simcoon"
 copyright = "2025, 3MAH development team"
 author = "3MAH development team"
 
-# The full version, including alpha/beta/rc tags
-release = "1.9.6"
+# Respect the release of simcoon
+# Default values
+release = version = "unknown"
 
+# Path to the version file
+version_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../python-setup/simcoon/__version__.py")
+)
+
+# Load the __version__ variable safely if the file exists
+if os.path.exists(version_path):
+    spec = importlib.util.spec_from_file_location("simcoon_version", version_path)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    release = getattr(mod, "__version__", "unknown")
+    version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,10 +48,12 @@ release = "1.9.6"
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
-    "sphinx.ext.mathjax",
+    "sphinx.ext.autosummary",
+    "sphinx_gallery.gen_gallery",
+    "sphinx_design",
 ]
-
 
 # MOCK_MODULES = ["numpy", "cadquery", "OCP"]
 # for mod_name in MOCK_MODULES:
@@ -54,7 +68,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -62,6 +76,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+# html_theme = 'sphinxdoc'
+html_logo = "_static/simcoonLogos_ss_fond.png"
 
 html_theme_options = {
     "canonical_url": "https://microgen.readthedocs.io/en/latest/",
@@ -79,9 +95,22 @@ html_theme_options = {
     # 'titles_only': False
 }
 
-html_logo = "_static/simcoon_logo_text_original.png"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- Sphinx Gallery Options
+sphinx_gallery_conf = {
+    # convert rst to md for ipynb
+    "pypandoc": True,
+    # path to your examples scripts
+    "examples_dirs": ["../examples/"],
+    # path where to save gallery generated examples
+    "gallery_dirs": ["examples"],
+    # Pattern to search for example files
+    "filename_pattern": r"\.py",
+    # Sort gallery example by file name instead of number of lines (default)
+    "within_subsection_order": FileNameSortKey,
+    "image_scrapers": ("matplotlib"),
+}
