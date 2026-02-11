6a73386ccc49a81f022af3cb125d29835f1f16ac
Kevin Marchais <kevinmarchais@gmail.com>
2026-02-11 09:30:09 +0100

    Use levels parameter in omp_set_nested fallback

--- include/simcoon/omp_compat.hpp
+++ include/simcoon/omp_compat.hpp
@@ -6,8 +6,7 @@ inline void omp_set_active_levels(int levels) {
 #if _OPENMP >= 201811
     omp_set_max_active_levels(levels);
 #else
-    (void)levels;
-    omp_set_nested(1);
+    omp_set_nested(levels > 0);
 #endif
 }
 #endif
