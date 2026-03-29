97428485864463234032cd3a6cbfff4b2c6cf0b4
Yves Chemisky <yves.chemisky@gmail.com>
2026-03-29 15:43:36 +0200

    Use LIBRARY_PREFIX for CMake paths [skip ci]
    Update conda.recipe/bld.bat to use %LIBRARY_PREFIX% for CMake install prefix and to explicitly set CMAKE_INCLUDE_PATH and CMAKE_LIBRARY_PATH to %LIBRARY_PREFIX%/include and %LIBRARY_PREFIX%/lib. This ensures CMake can locate headers and libraries correctly in the conda build environment on Windows.

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -13,7 +13,9 @@ echo Using pybind11 from: %PYBIND11_DIR%
 :: CONDA_BUILD env var is detected by CMakeLists.txt to enable SIMCOON_BUILD_PYTHON
 cmake -G "Visual Studio 17 2022" ^
       -S . -B build ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_INCLUDE_PATH=%LIBRARY_PREFIX%/include ^
+      -DCMAKE_LIBRARY_PATH=%LIBRARY_PREFIX%/lib ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DSIMCOON_BUILD_TESTS:BOOL=OFF ^
       -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" ^
